6688252a275a3dd1777922679bc812391511caa2
milo1000 <gieceker@gmail.com>
2022-08-21 11:02:01 +0200

    Minor refactor and cleanup
    Timer changed to Stopwatch, minor code cleanup,
    doxygen comments added

---
+++ lib/mzgl/util/Stopwatch.h
@@ -0,0 +1,48 @@
+//
+//  Timer.h
+//  mzgl
+//
+//  Helpful debugging and performance thing.
+//
+//  Created by Marek Bereza on 10/08/2022.
+//  Copyright © 2022 Marek Bereza. All rights reserved.
+//
+
+#pragma once
+#include <chrono>
+struct Stopwatch {
+
+    std::chrono::steady_clock::time_point startTime;
+
+    //------------------------------------------------------------------------------------------------------------------------------
+    ///
+    /// @brief Starts Stopwatch.
+    ///
+    //------------------------------------------------------------------------------------------------------------------------------
+    void start() {
+        startTime = std::chrono::steady_clock::now();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------------
+    ///
+    /// @brief Returns the time delta from the start.
+    ///
+    /// @return time delta in milliseconds
+    //------------------------------------------------------------------------------------------------------------------------------
+    std::uint64_t getTimeDeltaMs() {
+        auto duration = std::chrono::steady_clock::now() - startTime;
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+        return ms.count();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------------
+    ///
+    /// @brief Prints the time delta measured from the start.
+    ///
+    /// @param message Message to be printed along with the measured time delta.
+    //------------------------------------------------------------------------------------------------------------------------------
+    void printTimeDeltaMs(const std::string &message) {
+        auto ms = getTimeDeltaMs();
+        printf("%s took %d ms\n", message.c_str(), ms);
+    }
+};
