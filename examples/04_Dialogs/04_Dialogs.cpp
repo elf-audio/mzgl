6ccdf7289d543406a40e53948bced5d1f41ea859
Marek Bereza <bereza@gmail.com>
2022-09-12 11:35:57 +0100

    dialogs example

---
+++ examples/04_Dialogs/04_Dialogs.cpp
@@ -0,0 +1,152 @@
+
+#include "App.h"
+#include <cmath>
+#include "mzAssert.h"
+#include "Dialogs.h"
+
+/**
+ * Very very simple button ui element class
+ */
+class Button : public Layer {
+public:
+    Button(Graphics &g, const std::string &name) : Layer(g, name) {
+        interactive = true;
+    }
+
+    void draw() override {
+
+        g.fill();
+
+        if(down) {
+            g.setColor(0);
+            g.drawRoundedRect(*this, 5);
+        } else if(over) {
+            g.setColor(0.4);
+            g.drawRoundedRect(*this, 5);
+        }
+        
+
+        g.setColor(1);
+        g.noFill();
+        g.drawRoundedRect(*this, 5);
+        
+        g.drawTextCentred(name, centre());
+    }
+
+    std::function<void()> released = [](){};
+
+    bool over = false;
+    bool down = false;
+    void touchOver(float x, float y) override {
+        over = inside(x, y);
+    }
+    bool touchDown(float x, float y, int id) override {
+        down = true;
+        return true;
+    }
+
+    void touchMoved(float x, float y, int id) override {
+        down = inside(x, y);
+    }
+    void touchUp(float x, float y, int id) override {
+        if(inside(x, y)) {
+            released();
+        }
+        down = false;
+    }
+};
+
+
+
+
+class MyApp : public App {
+public:
+
+    Dialogs dialogs;
+    MyApp(Graphics &g) : App(g), dialogs(*this) {
+
+        auto alertButton            = new Button(g, "SHOW ALERT");
+        auto twoOptionCancelButton  = new Button(g, "SHOW TWO OPTION CANCEL");
+        auto chooseImageButton      = new Button(g, "CHOOSE IMAGE");
+        auto shareButton            = new Button(g, "SHARE");
+        auto loadButton             = new Button(g, "LOAD FILE");
+        auto launchUrlButton        = new Button(g, "LAUNCH URL");
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // actions
+
+        alertButton->released = [this]() {
+            dialogs.alert("TITLE", "MESSAGE");
+        };
+
+
+        twoOptionCancelButton->released = [this]() {
+            dialogs.twoOptionCancelDialog("TITLE", "MSG",
+                                   "BUTTON ONE", [this](){dialogs.alert("", "button one pressed");},
+                                   "BUTTON TWO", [this](){dialogs.alert("", "button two pressed");},
+
+                                   // cancel
+                                   [this](){dialogs.alert("", "cancel pressed");});
+        };
+
+
+
+
+        chooseImageButton->released = [this]() {
+            dialogs.chooseImage([this](bool success, std::string path) {
+                dialogs.alert("", "LOAD IMAGE - success: "+std::to_string(success) + " path: " + path);
+            });
+        };
+
+        shareButton->released = [this]() {
+            dialogs.share("SHARE THIS FILE", "/path/to/real/file", [this](bool success) {
+                dialogs.alert("", "SHARE FILE - success: "+std::to_string(success));
+            });
+        };
+
+
+        loadButton->released = [this]() {
+            dialogs.loadFile("CHOOSE A FILE", [this](std::string path, bool success) {
+                dialogs.alert("", "LOAD FILE - success: "+std::to_string(success) + " path: " + path);
+            });
+        };
+
+
+        launchUrlButton->released = [this]() {
+            dialogs.launchUrlInWebView("https://www.koalasampler.com", [](){});
+        };
+
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // do the layout
+        const float buttonWidth = g.width * 0.5;
+        const float buttonHeight = g.height * 0.1;
+        const float padding = 20;
+
+        alertButton->setFromCentre({g.width/2, buttonHeight}, buttonWidth, buttonHeight);
+
+
+        root->addChildren({alertButton, twoOptionCancelButton, chooseImageButton, shareButton, loadButton, launchUrlButton});
+        for(int i = 1; i < root->getNumChildren(); i++) {
+            root->getChild(i)->size(alertButton->size());
+            root->getChild(i)->positionUnder(root->getChild(i-1), padding);
+        }
+    }
+
+    void draw() override {
+        // grey background
+        g.clear(0.2);
+    }
+    
+};
+
+
+App *instantiateApp(Graphics &g) {
+
+    g.width = 800;
+    g.height = 800;
+
+    return new MyApp(g); 
+}
--- lib/mzgl/util/Dialogs.h
+++ lib/mzgl/util/Dialogs.h
@@ -15,34 +15,102 @@ class App;
 class Dialogs {
 public:
 	const App &app;
+
+	/**
+	 * Constructor must take the calling app because 
+	 * some dialog methods need references to the apps window
+	 * which can be stored in the App class.
+	 */
 	Dialogs(const App &app) : app(app) {
 		
 	}
+
 	Dialogs & operator=(const Dialogs&) = delete;
 	Dialogs(const Dialogs&) = delete;
 
-	void textbox(std::string title, std::string msg, std::string text, std::function<void(std::string, bool)> completionCallback) const;
-	
-	
-	
+
+
+
+
+
+
+
+	/**
+	 * launch an alert dialog - e.g. just a message with an ok button.
+	 *
+	 * @param title - can be ignored if the native window system doesn't
+	 *			      easily support titles on dialog windows.
+	 * 
+	 * @param msg - the message to be displayed
+	 */
+	void alert(std::string title, std::string msg) const;
+
+
+	/**
+	 * launch a dialog with a message, an ok and a cancel button.
+	 * 
+	 * @param title - can be ignored if the native window system doesn't
+	 *			      easily support titles on dialog windows.
+	 * 
+	 * @param msg - the message to be displayed.
+	 * 
+	 * @param okPressed - the callback if the user pressed "ok"
+	 * 
+	 * @param cancelPressed - the callback if the user pressed "cancel"
+	 */
 	void confirm(std::string title, std::string msg,
-	std::function<void()> okPressed,
-	std::function<void()> cancelPressed = [](){}) const;
+								std::function<void()> okPressed,
+								std::function<void()> cancelPressed = [](){}) const;
+
+	/**
+	 * launch a dialog with a message, a textbox and an ok and cancel button.
+	 *
+	 * @param title - can be ignored if the native window system doesn't
+	 *			      easily support titles on dialog windows.
+	 * 
+	 * @param msg - the message to be displayed.
+	 * 
+	 * @param text - the placeholder text that should be prepopulated into the textbox
+	 * 
+	 * @param completionCallback - the callback that should come back on the main thread to signal that
+	 *                             the user has pressed "ok" or "cancel" (the boolean argument)
+	 *                             and the string typed (the string argument)
+	 */
+	void textbox(std::string title, std::string msg, std::string text, 
+											std::function<void(std::string, bool)> completionCallback) const;
 	
+	
+	
+
+	
+	/**
+	 * launch a dialog with a message and 2 buttons.
+	 */
 	void twoOptionDialog(std::string title, std::string msg,
 							   std::string buttonOneText, std::function<void()> buttonOnePressed,
 							   std::string buttonTwoText, std::function<void()> buttonTwoPressed) const;
 	
+
+	/**
+	 * launch a dialog with a message and 2 buttons and a cancel button
+	 */
 	void twoOptionCancelDialog(std::string title, std::string msg,
 							   std::string buttonOneText, std::function<void()> buttonOnePressed,
 							   std::string buttonTwoText, std::function<void()> buttonTwoPressed,
 							   std::function<void()> cancelPressed) const;
 	
+
+	/**
+	 * launch a dialog with a message and 3 buttons
+	 */
 	void threeOptionDialog(std::string title, std::string msg,
 			std::string buttonOneText, std::function<void()> buttonOnePressed,
 			std::string buttonTwoText, std::function<void()> buttonTwoPressed,
 			  std::string buttonThreeText, std::function<void()> buttonThreePressed) const;
 
+	/**
+	 * launch a dialog with a message and 3 buttons and a cancel button
+	 */
 	void threeOptionCancelDialog(std::string title, std::string msg,
 			std::string buttonOneText, std::function<void()> buttonOnePressed,
 			std::string buttonTwoText, std::function<void()> buttonTwoPressed,
@@ -50,16 +118,34 @@ public:
 								 std::function<void()> cancelPressed) const;
 
 	
-	void alert(std::string title, std::string msg) const;
-	
+	/**
+	 * launch a dialog with a message and 3 buttons and a cancel button
+	 */
 	void chooseImage(std::function<void(bool success, std::string imgPath)> completionCallback) const;
 	
+	/**
+	 * If the OS has a standard sharing dialog, this method launches one of those.
+	 */
 	void share(std::string message, std::string path, std::function<void(bool)> completionCallback) const;
 	
+	/**
+	 * Launch file opening dialog
+	 */
 	void loadFile(std::string msg, std::function<void(std::string, bool)> completionCallback) const;
+
+	/**
+	 * Launch file opening dialog with allowed file extensions filter.
+	 */
 	void loadFile(std::string msg, const std::vector<std::string> &allowedExtensions, std::function<void(std::string, bool)> completionCallback) const;
 	
+	/**
+	 * launches a web view pointing to a web url - only really needed on iOS (for ableton lite download)
+	 */
 	void launchUrlInWebView(std::string url, std::function<void()> completionCallback) const;
+
+	/**
+	 * launch a webview that displays the html contained in the string. Optionally calls completionCallback if webview is dismissed
+	 */
 	void displayHtmlInWebView(const std::string &html, std::function<void()> completionCallback) const;
 };
 
