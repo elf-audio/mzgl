c6cb313e40a32ce30238b7c65ac03d7b057dedff
milo1000 <gieceker@gmail.com>
2023-01-04 18:24:45 +0100

    formatting

--- lib/mzgl/app/glfw/GLFWAppRunner.cpp
+++ lib/mzgl/app/glfw/GLFWAppRunner.cpp
@@ -39,24 +39,24 @@ bool leftShiftDown = false;
 bool rightShiftDown = false;
 
 EventDispatcher *getEventDispatcher(GLFWwindow *window) {
-	auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
-	return app->eventDispatcher;
+    auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
+    return app->eventDispatcher;
 }
 
 Graphics &getGraphics(GLFWwindow *window) {
-	auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
-	return app->graphics;
+    auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
+    return app->graphics;
 }
 
 
 static void error_callback(int error, const char* description) {
-	fprintf(stderr, "Error: %s\n", description);
+    fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	// if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	//    glfwSetWindowShouldClose(window, GLFW_TRUE);
-	if(action==GLFW_PRESS || action==GLFW_REPEAT) {
+    // if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    //    glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if(action==GLFW_PRESS || action==GLFW_REPEAT) {
         if(key==GLFW_KEY_LEFT_ALT) {
             leftAltDown = true;
         } else if(key==GLFW_KEY_RIGHT_ALT) {
@@ -66,9 +66,9 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         } else if(key==GLFW_KEY_RIGHT_SHIFT) {
             rightShiftDown = true;
         }
-		getEventDispatcher(window)->keyDown(key);
+        getEventDispatcher(window)->keyDown(key);
 
-	} else if(action==GLFW_RELEASE) {
+    } else if(action==GLFW_RELEASE) {
         if(key==GLFW_KEY_LEFT_ALT) {
             leftAltDown = false;
         } else if(key==GLFW_KEY_RIGHT_ALT) {
@@ -78,42 +78,42 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         } else if(key==GLFW_KEY_RIGHT_SHIFT) {
             rightShiftDown = false;
         }
-		getEventDispatcher(window)->keyUp(key);
-	}
+        getEventDispatcher(window)->keyUp(key);
+    }
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
 
-	if(action==GLFW_PRESS) {
-		mouseIsDown = true;
-		buttons[button] = true;
-		getEventDispatcher(window)->touchDown(mouseX, mouseY, button);
-	} else {
-		getEventDispatcher(window)->touchUp(mouseX, mouseY, button);
-		buttons[button] = false;
-		for(const auto &b : buttons) {
-			if(b) return;
-		}
-		mouseIsDown = false;
-	}
+    if(action==GLFW_PRESS) {
+        mouseIsDown = true;
+        buttons[button] = true;
+        getEventDispatcher(window)->touchDown(mouseX, mouseY, button);
+    } else {
+        getEventDispatcher(window)->touchUp(mouseX, mouseY, button);
+        buttons[button] = false;
+        for(const auto &b : buttons) {
+            if(b) return;
+        }
+        mouseIsDown = false;
+    }
 }
 
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-	auto &g = getGraphics(window);
-	mouseX = xpos*g.pixelScale;
-	mouseY = ypos*g.pixelScale;
-	if(!mouseIsDown) {
-		getEventDispatcher(window)->touchOver(mouseX, mouseY);
+    auto &g = getGraphics(window);
+    mouseX = xpos*g.pixelScale;
+    mouseY = ypos*g.pixelScale;
+    if(!mouseIsDown) {
+        getEventDispatcher(window)->touchOver(mouseX, mouseY);
 
-	} else {
-		for(int i = 0; i < buttons.size(); i++) {
-			if(buttons[i]) {
+    } else {
+        for(int i = 0; i < buttons.size(); i++) {
+            if(buttons[i]) {
 
-				getEventDispatcher(window)->touchMoved(mouseX, mouseY, i);
+                getEventDispatcher(window)->touchMoved(mouseX, mouseY, i);
 
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
@@ -138,50 +138,50 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
 
 void window_size_callback(GLFWwindow* window, int width, int height) {
 
-	auto &g = getGraphics(window);
+    auto &g = getGraphics(window);
     glViewport(0, 0, width, height);
 
-	g.width = width*g.pixelScale;
-	g.height = height*g.pixelScale;
+    g.width = width*g.pixelScale;
+    g.height = height*g.pixelScale;
 
     getEventDispatcher(window)->resized();
-	
-	Log::d() << "resized to " << width << height;
+
+    Log::d() << "resized to " << width << height;
 }
 
 void drop_callback(GLFWwindow* window, int count, const char** paths) {
-	for (int i = 0; i < count; i++) {
-		getEventDispatcher(window)->openUrl(paths[i]);
-	}
+    for (int i = 0; i < count; i++) {
+        getEventDispatcher(window)->openUrl(paths[i]);
+    }
 }
 
 
 float getMainMonitorScale() {
-	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-	if(monitor!=nullptr) {
-		float xscale, yscale;
-		glfwGetMonitorContentScale(monitor, &xscale, &yscale);
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    if(monitor!=nullptr) {
+        float xscale, yscale;
+        glfwGetMonitorContentScale(monitor, &xscale, &yscale);
         Log::d() << "scale x: " << xscale << " scale y: " << yscale;
-		return xscale;
-	} else {
-		printf("Error, can't find main monitor\n");
+        return xscale;
+    } else {
+        printf("Error, can't find main monitor\n");
 
-	}
-	return 1;
+    }
+    return 1;
 }
 
 
 void GLFWAppRunner::setCallbacks() {
-	if(window==nullptr) {
-		printf("ERROR! window not initialized yet!\n");
-	}
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetMouseButtonCallback(window, mouse_button_callback);
-	glfwSetCursorPosCallback(window, cursor_position_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-	glfwSetWindowSizeCallback(window, window_size_callback);
-	glfwSetDropCallback(window, drop_callback);
-	glfwSetWindowUserPointer(window, this);
+    if(window==nullptr) {
+        printf("ERROR! window not initialized yet!\n");
+    }
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetWindowSizeCallback(window, window_size_callback);
+    glfwSetDropCallback(window, drop_callback);
+    glfwSetWindowUserPointer(window, this);
 
 }
 
@@ -190,18 +190,18 @@ void GLFWAppRunner::setCallbacks() {
 void GLFWAppRunner::run(int argc, char *argv[]) {
 
 
-	loadCommandLineArgs(argc, (const char **)argv);
+    loadCommandLineArgs(argc, (const char **)argv);
 
-	for(int i =0; i < NUM_MOUSE_BUTTONS; i++) buttons.push_back(false);
-	
-	glfwSetErrorCallback(error_callback);
-	if (!glfwInit()) {
-		printf("Can't init GLFW\n");
-		exit(EXIT_FAILURE);
-	}
+    for(int i =0; i < NUM_MOUSE_BUTTONS; i++) buttons.push_back(false);
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        printf("Can't init GLFW\n");
+        exit(EXIT_FAILURE);
+    }
 
 
-	//graphics.pixelScale = getMainMonitorScale();
+    //graphics.pixelScale = getMainMonitorScale();
     graphics.pixelScale = 1.0f;
 
     Log::d() << "Pixel scale is " << graphics.pixelScale;
@@ -209,48 +209,48 @@ void GLFWAppRunner::run(int argc, char *argv[]) {
     // on linux window is really small, so lets bump it up.
     graphics.width *= graphics.pixelScale;
     graphics.height *= graphics.pixelScale;
-    	
-	//app = instantiateApp(graphics);
 
-	
-	glfwWindowHint(GLFW_SAMPLES, 16);
+    //app = instantiateApp(graphics);
+
+
+    glfwWindowHint(GLFW_SAMPLES, 16);
 
 #ifdef UNIT_TEST
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #else
     // this was set to 2.0 before, I bumped it to 3.2 so I can use imgui
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 #endif
 
-	app = instantiateApp(graphics);
-	
-	Log::d() << "Request window " << (graphics.width / graphics.pixelScale) << "x"
+    app = instantiateApp(graphics);
+
+    Log::d() << "Request window " << (graphics.width / graphics.pixelScale) << "x"
              << (graphics.height / graphics.pixelScale);
-	
+
     window = glfwCreateWindow(graphics.width/graphics.pixelScale, graphics.height/graphics.pixelScale, "mzgl", NULL, NULL);
 
-	int windowH, windowW;
+    int windowH, windowW;
     glfwGetWindowSize(window, &windowW, &windowH);
     Log::d() << "Window crated: " << windowW << "x" << windowH;
-    
-	glfwGetFramebufferSize(window, &windowW, &windowH);
+
+    glfwGetFramebufferSize(window, &windowW, &windowH);
     Log::d() << "FB crated: " << windowW << "x" << windowH;
 
 
     if (!window) {
-		printf("Can't create GLFW window\n");
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+        printf("Can't create GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	graphics.width = windowW * graphics.pixelScale;
+    graphics.width = windowW * graphics.pixelScale;
     graphics.height = windowH * graphics.pixelScale;
 
-	//app = instantiateApp(graphics);
+    //app = instantiateApp(graphics);
 
-	app->windowHandle = window;
+    app->windowHandle = window;
 
 #ifdef __linux__
     gtk_init(&argc, &argv);
@@ -260,44 +260,44 @@ void GLFWAppRunner::run(int argc, char *argv[]) {
     }
 #endif
 
-	setCallbacks();
-	
-	glfwSwapInterval(1);
-	// NOTE: OpenGL error checks have been omitted for brevity
-	glfwMakeContextCurrent(window);
+    setCallbacks();
 
-	// GL_MULTISAMPLE does not seem to exist on certain linux environment. 
-	// e.g. https://stackoverflow.com/questions/4207506/where-is-gl-multisample-defined
+    glfwSwapInterval(1);
+    // NOTE: OpenGL error checks have been omitted for brevity
+    glfwMakeContextCurrent(window);
+
+    // GL_MULTISAMPLE does not seem to exist on certain linux environment.
+    // e.g. https://stackoverflow.com/questions/4207506/where-is-gl-multisample-defined
 #ifdef GL_MULTISAMPLE
-	glEnable(GL_MULTISAMPLE);
+    glEnable(GL_MULTISAMPLE);
 #endif
 
 #ifdef _WIN32
-	GLenum err = glewInit();
-	if(err!=GLEW_OK) {
-		printf("Problem with glew\n");
-		return;
-	}
+    GLenum err = glewInit();
+    if(err!=GLEW_OK) {
+        printf("Problem with glew\n");
+        return;
+    }
 #endif
 
 
-	initMZGL(app);
+    initMZGL(app);
 
-	eventDispatcher = new EventDispatcher(app);
-	eventDispatcher->setup();
+    eventDispatcher = new EventDispatcher(app);
+    eventDispatcher->setup();
 
-	while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
 
-		eventDispatcher->runFrame();
+        eventDispatcher->runFrame();
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	eventDispatcher->exit();
+    eventDispatcher->exit();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
 #ifdef _WIN32
     delete app;
@@ -308,7 +308,7 @@ void GLFWAppRunner::run(int argc, char *argv[]) {
 }
 
 void GLFWAppRunner::stop() {
-	glfwSetWindowShouldClose(window, true);
+    glfwSetWindowShouldClose(window, true);
 }
 
 
