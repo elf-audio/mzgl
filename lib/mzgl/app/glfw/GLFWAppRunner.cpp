f43a9c635fc70948f0139f39cb2acc63c7a57133
milo1000 <gieceker@gmail.com>
2023-01-05 10:00:13 +0100

    win/lin scalling issues solved (#4)
    * scaling corrected
    
    * formatting cleanup
    
    * suppress excessive resize events
    
    * scissors cleanup
    
    Co-authored-by: milo1000 <“gieceker@gmail.com”>

--- lib/mzgl/app/glfw/GLFWAppRunner.cpp
+++ lib/mzgl/app/glfw/GLFWAppRunner.cpp
@@ -38,25 +38,27 @@ bool rightAltDown = false;
 bool leftShiftDown = false;
 bool rightShiftDown = false;
 
+bool framebuferResized = false;
+
 EventDispatcher *getEventDispatcher(GLFWwindow *window) {
-	auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
-	return app->eventDispatcher;
+    auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
+    return app->eventDispatcher;
 }
 
 Graphics &getGraphics(GLFWwindow *window) {
-	auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
-	return app->graphics;
+    auto *app = (GLFWAppRunner*)glfwGetWindowUserPointer(window);
+    return app->graphics;
 }
 
 
 static void error_callback(int error, const char* description) {
-	fprintf(stderr, "Error: %s\n", description);
+    fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	// if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	//    glfwSetWindowShouldClose(window, GLFW_TRUE);
-	if(action==GLFW_PRESS || action==GLFW_REPEAT) {
+    // if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    //    glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if(action==GLFW_PRESS || action==GLFW_REPEAT) {
         if(key==GLFW_KEY_LEFT_ALT) {
             leftAltDown = true;
         } else if(key==GLFW_KEY_RIGHT_ALT) {
@@ -66,9 +68,9 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         } else if(key==GLFW_KEY_RIGHT_SHIFT) {
             rightShiftDown = true;
         }
-		getEventDispatcher(window)->keyDown(key);
+        getEventDispatcher(window)->keyDown(key);
 
-	} else if(action==GLFW_RELEASE) {
+    } else if(action==GLFW_RELEASE) {
         if(key==GLFW_KEY_LEFT_ALT) {
             leftAltDown = false;
         } else if(key==GLFW_KEY_RIGHT_ALT) {
@@ -78,42 +80,42 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         } else if(key==GLFW_KEY_RIGHT_SHIFT) {
             rightShiftDown = false;
         }
-		getEventDispatcher(window)->keyUp(key);
-	}
+        getEventDispatcher(window)->keyUp(key);
+    }
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
 
-	if(action==GLFW_PRESS) {
-		mouseIsDown = true;
-		buttons[button] = true;
-		getEventDispatcher(window)->touchDown(mouseX, mouseY, button);
-	} else {
-		getEventDispatcher(window)->touchUp(mouseX, mouseY, button);
-		buttons[button] = false;
-		for(const auto &b : buttons) {
-			if(b) return;
-		}
-		mouseIsDown = false;
-	}
+    if(action==GLFW_PRESS) {
+        mouseIsDown = true;
+        buttons[button] = true;
+        getEventDispatcher(window)->touchDown(mouseX, mouseY, button);
+    } else {
+        getEventDispatcher(window)->touchUp(mouseX, mouseY, button);
+        buttons[button] = false;
+        for(const auto &b : buttons) {
+            if(b) return;
+        }
+        mouseIsDown = false;
+    }
 }
 
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-	auto &g = getGraphics(window);
-	mouseX = xpos*g.pixelScale;
-	mouseY = ypos*g.pixelScale;
-	if(!mouseIsDown) {
-		getEventDispatcher(window)->touchOver(mouseX, mouseY);
+    auto &g = getGraphics(window);
+    mouseX = xpos;
+    mouseY = ypos;
+    if(!mouseIsDown) {
+        getEventDispatcher(window)->touchOver(mouseX, mouseY);
 
-	} else {
-		for(int i = 0; i < buttons.size(); i++) {
-			if(buttons[i]) {
+    } else {
+        for(int i = 0; i < buttons.size(); i++) {
+            if(buttons[i]) {
 
-				getEventDispatcher(window)->touchMoved(mouseX, mouseY, i);
+                getEventDispatcher(window)->touchMoved(mouseX, mouseY, i);
 
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
@@ -138,49 +140,51 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
 
 void window_size_callback(GLFWwindow* window, int width, int height) {
 
-	auto &g = getGraphics(window);
+
+    auto &g = getGraphics(window);
     glViewport(0, 0, width, height);
 
-	g.width = width*g.pixelScale;
-	g.height = height*g.pixelScale;
+    g.width = width;
+    g.height = height;
 
-    getEventDispatcher(window)->resized();
-	
-	Log::d() << "resized to " << width << height;
+    framebuferResized = true; // just mark that we need to resize
+    Log::d() << "resized to " << width << height;
 }
 
 void drop_callback(GLFWwindow* window, int count, const char** paths) {
-	for (int i = 0; i < count; i++) {
-		getEventDispatcher(window)->openUrl(paths[i]);
-	}
+    for (int i = 0; i < count; i++) {
+        getEventDispatcher(window)->openUrl(paths[i]);
+    }
 }
 
 
 float getMainMonitorScale() {
-	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-	if(monitor!=nullptr) {
-		float xscale, yscale;
-		glfwGetMonitorContentScale(monitor, &xscale, &yscale);
-		return xscale;
-	} else {
-		printf("Error, can't find main monitor\n");
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    if(monitor!=nullptr) {
+        float xscale, yscale;
+        glfwGetMonitorContentScale(monitor, &xscale, &yscale);
+        Log::d() << "scale x: " << xscale << " scale y: " << yscale;
+        return xscale;
+    } else {
+        printf("Error, can't find main monitor\n");
 
-	}
-	return 1;
+    }
+    return 1;
 }
 
 
 void GLFWAppRunner::setCallbacks() {
-	if(window==nullptr) {
-		printf("ERROR! window not initialized yet!\n");
-	}
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetMouseButtonCallback(window, mouse_button_callback);
-	glfwSetCursorPosCallback(window, cursor_position_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-	glfwSetWindowSizeCallback(window, window_size_callback);
-	glfwSetDropCallback(window, drop_callback);
-	glfwSetWindowUserPointer(window, this);
+    if(window==nullptr) {
+        printf("ERROR! window not initialized yet!\n");
+    }
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    //glfwSetWindowSizeCallback(window, window_size_callback);
+    glfwSetFramebufferSizeCallback(window, window_size_callback);
+    glfwSetDropCallback(window, drop_callback);
+    glfwSetWindowUserPointer(window, this);
 
 }
 
@@ -189,94 +193,135 @@ void GLFWAppRunner::setCallbacks() {
 void GLFWAppRunner::run(int argc, char *argv[]) {
 
 
-	loadCommandLineArgs(argc, (const char **)argv);
+    loadCommandLineArgs(argc, (const char **)argv);
 
-	for(int i =0; i < NUM_MOUSE_BUTTONS; i++) buttons.push_back(false);
-	
-	glfwSetErrorCallback(error_callback);
-	if (!glfwInit()) {
-		printf("Can't init GLFW\n");
-		exit(EXIT_FAILURE);
-	}
+    for(int i =0; i < NUM_MOUSE_BUTTONS; i++) buttons.push_back(false);
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        printf("Can't init GLFW\n");
+        exit(EXIT_FAILURE);
+    }
 
 
-	graphics.pixelScale = getMainMonitorScale();
+    graphics.pixelScale = 1.0f; // actually we don't need any scaling, this is legacy code for compatibility
 
-    Log::d() << "Pixel scale is" << graphics.pixelScale;
-
-    app = instantiateApp(graphics);
-
-    // on linux window is really small, so lets bump it up.
-    graphics.width *= graphics.pixelScale;
-    graphics.height *= graphics.pixelScale;
-	
-	glfwWindowHint(GLFW_SAMPLES, 16);
+    glfwWindowHint(GLFW_SAMPLES, 16);
 
 #ifdef UNIT_TEST
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #else
     // this was set to 2.0 before, I bumped it to 3.2 so I can use imgui
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 #endif
 
-    window = glfwCreateWindow(graphics.width/graphics.pixelScale, graphics.height/graphics.pixelScale, "mzgl", NULL, NULL);
+    int requestedWidth = -1;
+    int requestedHeight = -1;
+
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    if (primaryMonitor != nullptr) {
+         const GLFWvidmode* currentVideoMode = glfwGetVideoMode(primaryMonitor);
+         if (currentVideoMode != nullptr) {
+            float h = (currentVideoMode->height) * 0.8; // make it 0.9 of max height, so there is room for decorations
+            float w = h * 0.54; // set width as 0.54 of height, that looks OK
+            requestedHeight = (int)h;
+            requestedWidth = (int)w;
+            if (requestedWidth > currentVideoMode->width) {
+                requestedWidth = currentVideoMode->width; // clamp in case of pivoted monitor
+            }
+         }
+    }
+
+    // Note that graphics object is not fully functional here
+    // as we need to update width/height later on.
+    app = instantiateApp(graphics);
+
+    if (requestedWidth != -1) {
+        graphics.width = requestedWidth;
+        graphics.height = requestedHeight;
+    }
+
+    Log::d() << "Request window " << (graphics.width) << "x"
+             << (graphics.height);
+
+    window = glfwCreateWindow(graphics.width, graphics.height, "mzgl", NULL, NULL);
+
+    int windowH, windowW;
+    glfwGetWindowSize(window, &windowW, &windowH); // note that it can be DIFFERENT than requested
+    Log::d() << "Window crated: " << windowW << "x" << windowH;
+
+    // Use Frame Buffer pixel size as graphics context size
+    // That would be compatible with GL functions using pixel coords (glViewport, glScissors, etc.)
+    glfwGetFramebufferSize(window, &windowW, &windowH);
+    Log::d() << "FB size: " << windowW << "x" << windowH;
 
     if (!window) {
-		printf("Can't create GLFW window\n");
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+        printf("Can't create GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	app->windowHandle = window;
+    graphics.width = windowW;
+    graphics.height = windowH;
+
+    app->windowHandle = window;
 
 #ifdef __linux__
     gtk_init(&argc, &argv);
+#endif
 
     if(argc>0) {
         glfwSetWindowTitle(window, fs::path(argv[0]).filename().string().c_str());
     }
-#endif
 
-	setCallbacks();
-	
-	glfwSwapInterval(1);
-	// NOTE: OpenGL error checks have been omitted for brevity
-	glfwMakeContextCurrent(window);
 
-	// GL_MULTISAMPLE does not seem to exist on certain linux environment. 
-	// e.g. https://stackoverflow.com/questions/4207506/where-is-gl-multisample-defined
+    setCallbacks();
+
+    glfwSwapInterval(1);
+    // NOTE: OpenGL error checks have been omitted for brevity
+    glfwMakeContextCurrent(window);
+
+    // GL_MULTISAMPLE does not seem to exist on certain linux environment.
+    // e.g. https://stackoverflow.com/questions/4207506/where-is-gl-multisample-defined
 #ifdef GL_MULTISAMPLE
-	glEnable(GL_MULTISAMPLE);
+    glEnable(GL_MULTISAMPLE);
 #endif
 
 #ifdef _WIN32
-	GLenum err = glewInit();
-	if(err!=GLEW_OK) {
-		printf("Problem with glew\n");
-		return;
-	}
+    GLenum err = glewInit();
+    if(err!=GLEW_OK) {
+        printf("Problem with glew\n");
+        return;
+    }
 #endif
 
 
-	initMZGL(app);
+    initMZGL(app);
 
-	eventDispatcher = new EventDispatcher(app);
-	eventDispatcher->setup();
+    eventDispatcher = new EventDispatcher(app);
+    eventDispatcher->setup();
 
-	while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
 
-		eventDispatcher->runFrame();
+        eventDispatcher->runFrame();
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
-	eventDispatcher->exit();
+        // While resizing glfwPollEvents blocks until user finishes action, so we can dispatch event here
+        // This is optimization to avoid dispatching from every resize callback, which can be overkill
+        if (framebuferResized) {
+            getEventDispatcher(window)->resized();
+            framebuferResized = false;
+        }
+    }
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    eventDispatcher->exit();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
 #ifdef _WIN32
     delete app;
@@ -287,7 +332,7 @@ void GLFWAppRunner::run(int argc, char *argv[]) {
 }
 
 void GLFWAppRunner::stop() {
-	glfwSetWindowShouldClose(window, true);
+    glfwSetWindowShouldClose(window, true);
 }
 
 
--- lib/mzgl/gl/Graphics.h
+++ lib/mzgl/gl/Graphics.h
@@ -37,250 +37,244 @@ class ScopedTransform;
 
 class Graphics {
 public:
-	int width = 0;
-	int height = 0;
-	float pixelScale = 2.f;
-	double frameDelta = 0.f;
-	double currFrameTime = 0.f;
-	
-	void setColor(float r, float g, float b, float a = 1);
-	void setColor(glm::vec3 c);
-	void setColor(glm::vec4 c);
-	void setColor(glm::vec4 c, float alpha);
-	void setColor(float bri);
-	void setHexColor(int hex, float a = 1);
-	void saveScreen(std::string pngPath);
+    int width = 0;
+    int height = 0;
+    float pixelScale = 2.f;
+    double frameDelta = 0.f;
+    double currFrameTime = 0.f;
 
-	std::function<void(bool)> setAntialiasing = [](bool){};
-	
-	void setBlending(bool shouldBlend);
-	bool isBlending();
-	glm::vec4 getColor();
+    void setColor(float r, float g, float b, float a = 1);
+    void setColor(glm::vec3 c);
+    void setColor(glm::vec4 c);
+    void setColor(glm::vec4 c, float alpha);
+    void setColor(float bri);
+    void setHexColor(int hex, float a = 1);
+    void saveScreen(std::string pngPath);
 
-	void clear(float c);
-	void clear(float r, float g, float b, float a = 1);
-	void clear(glm::vec4 c);
-	void clear(glm::vec3 c);
-	
-	Shader *currShader = nullptr;
-	
-	// these are the default shaders
-	ShaderRef nothingShader;
-	ShaderRef colorShader;
-	ShaderRef colorTextureShader;
-	ShaderRef texShader;
-	ShaderRef fontShader;
-	
-	void loadDefaultShaders();
+    std::function<void(bool)> setAntialiasing = [](bool){};
 
+    void setBlending(bool shouldBlend);
+    bool isBlending();
+    glm::vec4 getColor();
 
-	
-	void noFill();
-	void fill();
-	bool isFilling();
+    void clear(float c);
+    void clear(float r, float g, float b, float a = 1);
+    void clear(glm::vec4 c);
+    void clear(glm::vec3 c);
 
-	void setStrokeWeight(float f);
-	float getStrokeWeight();
-	
-		
-	/////////////////////////////////////////////////////////////////////////////
-	// SHAPES
+    Shader *currShader = nullptr;
 
-	void drawRect(float x, float y, float width, float height);
-	void drawRect(const Rectf &r);
-	void draw(const Rectf &r);
+    // these are the default shaders
+    ShaderRef nothingShader;
+    ShaderRef colorShader;
+    ShaderRef colorTextureShader;
+    ShaderRef texShader;
+    ShaderRef fontShader;
 
-	// this is very slow - use the RoundedRect class instead to cache
-	void drawRoundedRect(const Rectf &r, float radius);
-	void drawRoundedRectShadow(Rectf r, float radius, float shadow);
-	void draw(const Rectf &r, float radius);
-	
-	void drawCircle(float x, float y, float r);
-	void drawCircle(glm::vec2 c, float r);
-	void drawArc(glm::vec2 c, float r, float startAngle, float endAngle);
-	void drawLine(glm::vec2 a, glm::vec2 b);
-	void drawLine(float x1, float y1, float x2, float y2);
-	void drawLineStrip(const std::vector<vec2> &pts);
-	void drawPlus(vec2 c, int diameter, int thickness);
-
-	void drawCross(vec2 c, int diameter, int thickness);
-	
-	void drawChevronLeft(vec2 c, int radius, int thickness);
-	void drawChevronRight(vec2 c, int radius, int thickness);
-	void drawChevronUp(vec2 c, int radius, int thickness);
-	void drawChevronDown(vec2 c, int radius, int thickness);
-
-	void drawShape(const std::vector<vec2> &shape);
-	void drawTriangle(vec2 a, vec2 b, vec2 c);
-	void drawVerts(const std::vector<glm::vec2> &verts, int type = GL_TRIANGLES);
-	void drawVerts(const std::vector<glm::vec2> &verts, const std::vector<uint32_t> &indices);
-	void drawVerts(const std::vector<glm::vec2> &verts, const std::vector<glm::vec4> &cols, int type = GL_TRIANGLES);
-
-	void drawText(const std::string &s, float x, float y);
-	void drawText(const std::string &s, glm::vec2 p);
-	void drawTextWithBG(const std::string &s, vec4 bgColor, float x, float y);
-	void drawTextCentred(const std:: string &s, glm::vec2 c);
-	void drawTextVerticallyCentred(const std::string &text, glm::vec2 c);
-	void drawTextHorizontallyCentred(const std::string &text, glm::vec2 c);
-	
-	/////////////////////////////////////////////////////////////////////////////
-	// MATRIX STUFF
-	void pushMatrix();
-	void popMatrix();
+    void loadDefaultShaders();
 
 
-	
-	
-	void loadIdentity();
-	void translate(glm::vec2 d);
-	void translate(glm::vec3 d);
 
-	
-	void translate(float x, float y, float z = 0);
-	void scale(float amt);
-	void scale(float x, float y, float z = 1);
+    void noFill();
+    void fill();
+    bool isFilling();
 
-	void rotate(float angle, glm::vec3 axis);
-	void rotateX(float angle);
-	void rotateY(float angle);
-	void rotateZ(float angle);
-	void setProjectionMatrix(glm::mat4 projMat);
+    void setStrokeWeight(float f);
+    float getStrokeWeight();
 
-	void setViewMatrix(glm::mat4 viewMat);
-	glm::mat4 getMVP();
-	const glm::mat4 &getModelMatrix();
-	/////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////
-	// PRIVATE
-	void initGraphics();
-	void setupView(bool flipped = true, int w = 0, int h = 0);
-	void setupViewOrtho(float w = 0.f, float h = 0.f);
-	unsigned int getFrameNum() { return frameNum; }
-	// when you want to unbind a frame buffer (fbo) you need to bind to this
-	// as on iOS, the default framebuffer is not always 0
-	GLint getDefaultFrameBufferId();
-	Font &getFont();
-	void unloadFont();
-	Font *font = nullptr;
-	std::vector<unsigned char> getDefaultFontTTFData();
-	
-	
-	bool firstFrame = true;
 
-	// this function will warp a rect so it works with glScissor
-	// - different OS's have different quirks regarding this
-	void warpMaskForScissor(Rectf &a);
+    /////////////////////////////////////////////////////////////////////////////
+    // SHAPES
 
-	// currently focused layer once a touch has gone down
-	// this should be replaced by a touch id to Layer* map for multitouch
-	// needs to be here so it's not static
-	std::map<int,Layer*> focusedLayers;
+    void drawRect(float x, float y, float width, float height);
+    void drawRect(const Rectf &r);
+    void draw(const Rectf &r);
 
-	friend class App;
-		
+    // this is very slow - use the RoundedRect class instead to cache
+    void drawRoundedRect(const Rectf &r, float radius);
+    void drawRoundedRectShadow(Rectf r, float radius, float shadow);
+    void draw(const Rectf &r, float radius);
+
+    void drawCircle(float x, float y, float r);
+    void drawCircle(glm::vec2 c, float r);
+    void drawArc(glm::vec2 c, float r, float startAngle, float endAngle);
+    void drawLine(glm::vec2 a, glm::vec2 b);
+    void drawLine(float x1, float y1, float x2, float y2);
+    void drawLineStrip(const std::vector<vec2> &pts);
+    void drawPlus(vec2 c, int diameter, int thickness);
+
+    void drawCross(vec2 c, int diameter, int thickness);
+
+    void drawChevronLeft(vec2 c, int radius, int thickness);
+    void drawChevronRight(vec2 c, int radius, int thickness);
+    void drawChevronUp(vec2 c, int radius, int thickness);
+    void drawChevronDown(vec2 c, int radius, int thickness);
+
+    void drawShape(const std::vector<vec2> &shape);
+    void drawTriangle(vec2 a, vec2 b, vec2 c);
+    void drawVerts(const std::vector<glm::vec2> &verts, int type = GL_TRIANGLES);
+    void drawVerts(const std::vector<glm::vec2> &verts, const std::vector<uint32_t> &indices);
+    void drawVerts(const std::vector<glm::vec2> &verts, const std::vector<glm::vec4> &cols, int type = GL_TRIANGLES);
+
+    void drawText(const std::string &s, float x, float y);
+    void drawText(const std::string &s, glm::vec2 p);
+    void drawTextWithBG(const std::string &s, vec4 bgColor, float x, float y);
+    void drawTextCentred(const std:: string &s, glm::vec2 c);
+    void drawTextVerticallyCentred(const std::string &text, glm::vec2 c);
+    void drawTextHorizontallyCentred(const std::string &text, glm::vec2 c);
+
+    /////////////////////////////////////////////////////////////////////////////
+    // MATRIX STUFF
+    void pushMatrix();
+    void popMatrix();
+
+
+
+
+    void loadIdentity();
+    void translate(glm::vec2 d);
+    void translate(glm::vec3 d);
+
+
+    void translate(float x, float y, float z = 0);
+    void scale(float amt);
+    void scale(float x, float y, float z = 1);
+
+    void rotate(float angle, glm::vec3 axis);
+    void rotateX(float angle);
+    void rotateY(float angle);
+    void rotateZ(float angle);
+    void setProjectionMatrix(glm::mat4 projMat);
+
+    void setViewMatrix(glm::mat4 viewMat);
+    glm::mat4 getMVP();
+    const glm::mat4 &getModelMatrix();
+    /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
+    // PRIVATE
+    void initGraphics();
+    void setupView(bool flipped = true, int w = 0, int h = 0);
+    void setupViewOrtho(float w = 0.f, float h = 0.f);
+    unsigned int getFrameNum() { return frameNum; }
+    // when you want to unbind a frame buffer (fbo) you need to bind to this
+    // as on iOS, the default framebuffer is not always 0
+    GLint getDefaultFrameBufferId();
+    Font &getFont();
+    void unloadFont();
+    Font *font = nullptr;
+    std::vector<unsigned char> getDefaultFontTTFData();
+
+
+    bool firstFrame = true;
+
+    // this function will warp a rect so it works with glScissor
+    // - different OS's have different quirks regarding this
+    void warpMaskForScissor(Rectf &a);
+
+    // currently focused layer once a touch has gone down
+    // this should be replaced by a touch id to Layer* map for multitouch
+    // needs to be here so it's not static
+    std::map<int,Layer*> focusedLayers;
+
+    friend class App;
+
 private:
-	float strokeWeight = 1;
-	GLint defaultFBO;
-	bool blendingEnabled = false;
-	bool filling = true;
-	glm::vec4 color;
-	//glm::mat4 mvp;
-	MatrixStack modelMatrixStack;
-	glm::mat4 projectionMatrix;
-	glm::mat4 viewMatrix;
-	// this is cached version of the above multiplied
-	glm::mat4 viewProjectionMatrix;
-	friend class ScopedAlphaBlend;
-	friend class ScopedTranslate;
-	
-	GLuint immediateVertexArray = 0;
-	GLuint immediateVertexBuffer = 0;
-	GLuint immediateColorBuffer = 0;
-	GLuint immediateIndexBuffer = 0;
-	
-	
-	// was in Globals
-	unsigned int frameNum = 0;
+    float strokeWeight = 1;
+    GLint defaultFBO;
+    bool blendingEnabled = false;
+    bool filling = true;
+    glm::vec4 color;
+    //glm::mat4 mvp;
+    MatrixStack modelMatrixStack;
+    glm::mat4 projectionMatrix;
+    glm::mat4 viewMatrix;
+    // this is cached version of the above multiplied
+    glm::mat4 viewProjectionMatrix;
+    friend class ScopedAlphaBlend;
+    friend class ScopedTranslate;
+
+    GLuint immediateVertexArray = 0;
+    GLuint immediateVertexBuffer = 0;
+    GLuint immediateColorBuffer = 0;
+    GLuint immediateIndexBuffer = 0;
+
+
+    // was in Globals
+    unsigned int frameNum = 0;
 };
 
 class ScopedAlphaBlend {
 public:
-	Graphics &g;
-	ScopedAlphaBlend(Graphics &g, bool shouldBlend);
-	virtual ~ScopedAlphaBlend();
+    Graphics &g;
+    ScopedAlphaBlend(Graphics &g, bool shouldBlend);
+    virtual ~ScopedAlphaBlend();
 private:
-	bool originalBlendState;
-	
-	
+    bool originalBlendState;
+
+
 };
 
 
 struct ScopedTranslate {
-	Graphics &g;
-	ScopedTranslate(Graphics &g, glm::vec2 p) : g(g) { g.pushMatrix(); g.translate(p.x, p.y); }
-	ScopedTranslate(Graphics &g, float x, float y) : g(g) { g.pushMatrix(); g.translate(x, y); }
-	virtual ~ScopedTranslate() { g.popMatrix(); }
+    Graphics &g;
+    ScopedTranslate(Graphics &g, glm::vec2 p) : g(g) { g.pushMatrix(); g.translate(p.x, p.y); }
+    ScopedTranslate(Graphics &g, float x, float y) : g(g) { g.pushMatrix(); g.translate(x, y); }
+    virtual ~ScopedTranslate() { g.popMatrix(); }
 };
 struct ScopedTransform {
-	Graphics &g;
-	ScopedTransform(Graphics &g) : g(g) { g.pushMatrix(); }
-	virtual ~ScopedTransform() { g.popMatrix(); }
+    Graphics &g;
+    ScopedTransform(Graphics &g) : g(g) { g.pushMatrix(); }
+    virtual ~ScopedTransform() { g.popMatrix(); }
 };
 
 
 struct ScopedShaderEnable {
-	ShaderRef sh;
-	ScopedShaderEnable(ShaderRef shader) : sh(shader) {sh->begin();}
-	virtual ~ScopedShaderEnable() { sh->end(); }
+    ShaderRef sh;
+    ScopedShaderEnable(ShaderRef shader) : sh(shader) {sh->begin();}
+    virtual ~ScopedShaderEnable() { sh->end(); }
 };
 
 struct ScopedMask {
-	bool scissorWasEnabled = false;
-	float vals[4];
-	bool masking = false;
-	
-	// you can initialize with empty constructor
-	// so there's no mask actually happening
-	// this is handy when you want conditinal masks
-	ScopedMask() {
-		masking = false;
-	}
-	ScopedMask(Graphics &g, const Rectf &r) {
-		startMask(g, r);
-	}
+    bool scissorWasEnabled = false;
+    float vals[4];
+    bool masking = false;
 
-	void startMask(Graphics &g, const Rectf &r) {
-		masking = true;
-		if(glIsEnabled(GL_SCISSOR_TEST)) {
-			scissorWasEnabled = true;
-			glGetFloatv(GL_SCISSOR_BOX, vals);
-		}
-		glEnable(GL_SCISSOR_TEST);
+    // you can initialize with empty constructor
+    // so there's no mask actually happening
+    // this is handy when you want conditinal masks
+    ScopedMask() {
+        masking = false;
+    }
+    ScopedMask(Graphics &g, const Rectf &r) {
+        startMask(g, r);
+    }
 
-#if defined(__linux__) && !defined(__ANDROID__)
-		if(g.pixelScale!=1) {
-			auto a = r;
-			//g.warpMaskForScissor(a);
+    void startMask(Graphics &g, const Rectf &r) {
+        masking = true;
+        if(glIsEnabled(GL_SCISSOR_TEST)) {
+            scissorWasEnabled = true;
+            glGetFloatv(GL_SCISSOR_BOX, vals);
+        }
+        glEnable(GL_SCISSOR_TEST);
 
-			glScissor(a.x, g.height-(a.bottom()), a.width, a.height);
-		} else {
-			glScissor(r.x, g.height-(r.bottom()), r.width, r.height);
-		}
-#else
-		glScissor(r.x, g.height-(r.bottom()), r.width, r.height);
-#endif
-	}
-	
-	void stopMask() {
-		if(!masking) return;
-		if(scissorWasEnabled) {
-			glScissor(vals[0], vals[1], vals[2], vals[3]);
-		} else {
-			glDisable(GL_SCISSOR_TEST);
-		}
-	}
-	
-	virtual ~ScopedMask() {
-		stopMask();
-	}
+        // glScissor works with pixel coordinates
+        // so make sure that parameters are not
+        // in different coord-space. If thats the case,
+        // some kind of translation must take place here
+        // see: wrapMaskForScissors
+        glScissor(r.x, g.height-(r.bottom()), r.width, r.height);
+    }
+
+    void stopMask() {
+        if(!masking) return;
+        if(scissorWasEnabled) {
+            glScissor(vals[0], vals[1], vals[2], vals[3]);
+        } else {
+            glDisable(GL_SCISSOR_TEST);
+        }
+    }
+
+    virtual ~ScopedMask() {
+        stopMask();
+    }
 };
