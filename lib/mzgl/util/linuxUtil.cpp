f2f16f425c3947e16abcc9f41d5653726db90ee4
milo1000 <gieceker@gmail.com>
2023-01-26 18:48:05 +0100

    Lin: GTK dialogs fixed
    - dialog destruction fixed
    - bit of cleanup
    - clang-format

--- lib/mzgl/util/linuxUtil.cpp
+++ lib/mzgl/util/linuxUtil.cpp
@@ -1,6 +1,5 @@
 
 
-
 /*
 
 Originally compiled with
@@ -13,260 +12,237 @@ g++ main.cpp `pkg-config --libs --cflags gtk+-2.0`
 
 #include <stdio.h>
 
-
 #include <gtk/gtk.h>
-
 
 using namespace std;
 
 void linuxSaveFileDialog(string msg, string defaultFileName, function<void(string, bool)> completionCallback) {
+	GtkWidget *dialog = gtk_file_chooser_dialog_new(
+		msg.c_str(), nullptr, GTK_FILE_CHOOSER_ACTION_SAVE, "Cancel", GTK_RESPONSE_CANCEL, "Save", GTK_RESPONSE_ACCEPT, nullptr);
+	GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
 
-    GtkWidget *dialog = gtk_file_chooser_dialog_new(msg.c_str(),
-                                                    nullptr,
-                                                    GTK_FILE_CHOOSER_ACTION_SAVE,
-                                                    "Cancel",
-                                                    GTK_RESPONSE_CANCEL,
-                                                    "Save",
-                                                    GTK_RESPONSE_ACCEPT,
-                                                    nullptr);
-    GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
+	gtk_file_chooser_set_do_overwrite_confirmation(chooser, true);
 
-    gtk_file_chooser_set_do_overwrite_confirmation(chooser, true);
+	gtk_file_chooser_set_current_name(chooser, defaultFileName.c_str());
 
-    gtk_file_chooser_set_current_name(chooser, defaultFileName.c_str());
+	auto res = gtk_dialog_run(GTK_DIALOG(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
 
-    auto res = gtk_dialog_run(GTK_DIALOG (dialog));
-    while (g_main_context_iteration(nullptr, false));
+	if (res == GTK_RESPONSE_ACCEPT) {
+		char *filename;
 
-    if (res == GTK_RESPONSE_ACCEPT) {
-        char *filename;
+		filename = gtk_file_chooser_get_filename(chooser);
 
-        filename = gtk_file_chooser_get_filename(chooser);
-
-        completionCallback(filename, true);
-        g_free(filename);
-    } else {
-        completionCallback("", false);
-    }
-    gtk_widget_destroy(dialog);
+		completionCallback(filename, true);
+		g_free(filename);
+	} else {
+		completionCallback("", false);
+	}
+	gtk_widget_destroy(dialog);
 }
 
+void linuxLoadFileDialog(string msg, const vector<string> &allowedExtensions, function<void(string, bool)> completionCallback) {
+	GtkWidget *dialog = gtk_file_chooser_dialog_new(
+		msg.c_str(), nullptr, GTK_FILE_CHOOSER_ACTION_OPEN, "Cancel", GTK_RESPONSE_CANCEL, "Open", GTK_RESPONSE_ACCEPT, nullptr);
+	GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
 
-void linuxLoadFileDialog(string msg, const vector<string> &allowedExtensions,
-                         function<void(string, bool)> completionCallback) {
+	if (!allowedExtensions.empty()) {
+		auto *filter = gtk_file_filter_new();
+		gtk_file_filter_set_name(filter, "allowed files");
+		for (const auto &ext: allowedExtensions) {
+			string glob = "*." + ext;
+			gtk_file_filter_add_pattern(filter, glob.c_str());
+		}
+		gtk_file_chooser_add_filter(chooser, filter);
+	}
 
-    GtkWidget *dialog = gtk_file_chooser_dialog_new(msg.c_str(),
-                                                    nullptr,
-                                                    GTK_FILE_CHOOSER_ACTION_OPEN,
-                                                    "Cancel",
-                                                    GTK_RESPONSE_CANCEL,
-                                                    "Open",
-                                                    GTK_RESPONSE_ACCEPT,
-                                                    nullptr);
-    GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
+	auto res = gtk_dialog_run(GTK_DIALOG(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
 
+	char *filename = nullptr;
+	bool success = false;
 
-    if (!allowedExtensions.empty()) {
-        auto *filter = gtk_file_filter_new();
-        gtk_file_filter_set_name(filter, "allowed files");
-        for (const auto &ext : allowedExtensions) {
-            string glob = "*." + ext;
-            gtk_file_filter_add_pattern(filter, glob.c_str());
-        }
-        gtk_file_chooser_add_filter(chooser, filter);
-    }
+	if (res == GTK_RESPONSE_ACCEPT) {
+		filename = gtk_file_chooser_get_filename(chooser);
+		success = true;
+	}
 
+	gtk_widget_destroy(dialog);
+	while (g_main_context_iteration(nullptr, false))
+		; // to handle outstanding gtk events
 
-    auto res = gtk_dialog_run(GTK_DIALOG (dialog));
-    while (g_main_context_iteration(nullptr, false));
-    if (res == GTK_RESPONSE_ACCEPT) {
-        char *filename;
-        GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-        filename = gtk_file_chooser_get_filename(chooser);
-        completionCallback(filename, true);
-        g_free(filename);
-    } else {
-        completionCallback("", false);
-    }
-// maybe we need this if the dialog doesn't go away?
-    //gtk_widget_destroy (dialog);
+	completionCallback(success ? filename : "", success);
+	g_free(filename);
 }
 
-void linuxTextboxDialog(std::string title, std::string msg, std::string text,
-                        function<void(string, bool)> completionCallback) {
+void linuxTextboxDialog(std::string title, std::string msg, std::string text, function<void(string, bool)> completionCallback) {
+	GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(), nullptr, (GtkDialogFlags) 0, "OK", 1, "Cancel", 2, nullptr);
 
-    GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
-                                                    nullptr, (GtkDialogFlags) 0,
-                                                    "OK", 1,
-                                                    "Cancel", 2,
-                                                    nullptr);
+	GtkWidget *label = gtk_label_new(msg.c_str());
 
-    GtkWidget *label = gtk_label_new(msg.c_str());
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
 
-    gtk_container_add(GTK_CONTAINER (gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
+	GtkWidget *textbox = gtk_entry_new();
+	gtk_entry_set_max_length(GTK_ENTRY(textbox), 0);
+	gtk_entry_set_text(GTK_ENTRY(textbox), text.c_str());
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), textbox);
 
-    GtkWidget *textbox = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY (textbox), 0);
-    gtk_entry_set_text(GTK_ENTRY(textbox), text.c_str());
-    gtk_container_add(GTK_CONTAINER (gtk_dialog_get_content_area(GTK_DIALOG(dialog))), textbox);
+	gtk_widget_show_all(dialog);
 
-    gtk_widget_show_all(dialog);
-    // For some reason, for dialogs made with gtk_dialog_new_with_buttons
-    // this is how to destroy it (rather than gtk_widget_destroy)
-    g_signal_connect_swapped (dialog,
-                              "response",
-                              G_CALLBACK (gtk_widget_destroy),
-                              dialog);
+	auto result = gtk_dialog_run(GTK_DIALOG(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
+	string s = "";
+	if (result == 1) {
+		s = gtk_entry_get_text(GTK_ENTRY(textbox));
+	}
 
-    auto result = gtk_dialog_run(GTK_DIALOG(dialog));
-    while (g_main_context_iteration(nullptr, false));
-    string s = "";
-    if (result == 1) {
-        s = gtk_entry_get_text(GTK_ENTRY(textbox));
-    }
-   // Log::d() << "Destroying dialog";
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
 
-   // gtk_widget_destroy(GTK_WIDGET(dialog));
-    //gtk_window_close(GTK_WINDOW(dialog));
-   // Log::d() << "Destroyed dialog";
-    completionCallback(s, result == 1);
+	completionCallback(s, result == 1);
 }
-
 
 void linuxAlertDialog(string title, string msg) {
-
-    auto type = GTK_MESSAGE_INFO;
-    auto buttons = GTK_BUTTONS_OK;
-    GtkWidget *dialog = gtk_message_dialog_new(nullptr,
-                                               GTK_DIALOG_MODAL,
-                                               type,
-                                               buttons,
-                                               "%s", msg.c_str());
-    gtk_window_set_title(GTK_WINDOW(dialog), title.c_str());
-    gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-    while (g_main_context_iteration(nullptr, false));
+	auto type = GTK_MESSAGE_INFO;
+	auto buttons = GTK_BUTTONS_OK;
+	GtkWidget *dialog = gtk_message_dialog_new(nullptr, GTK_DIALOG_MODAL, type, buttons, "%s", msg.c_str());
+	gtk_window_set_title(GTK_WINDOW(dialog), title.c_str());
+	gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
 }
 
-
-void linuxConfirmDialog(std::string title, std::string msg,
-                        std::function<void()> okPressed,
-                        std::function<void()> cancelPressed) {
-
-    auto type = GTK_MESSAGE_QUESTION;
-    auto buttons = GTK_BUTTONS_OK_CANCEL;
-    GtkWidget *dialog = gtk_message_dialog_new(nullptr,
-                                               GTK_DIALOG_MODAL,
-                                               type,
-                                               buttons,
-                                               "%s", msg.c_str());
-    gtk_window_set_title(GTK_WINDOW(dialog), title.c_str());
-    auto result = gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-    while (g_main_context_iteration(nullptr, false));
-    if (result == GTK_RESPONSE_OK) {
-        okPressed();
-    } else {
-        cancelPressed();
-    }
+void linuxConfirmDialog(std::string title, std::string msg, std::function<void()> okPressed, std::function<void()> cancelPressed) {
+	auto type = GTK_MESSAGE_QUESTION;
+	auto buttons = GTK_BUTTONS_OK_CANCEL;
+	GtkWidget *dialog = gtk_message_dialog_new(nullptr, GTK_DIALOG_MODAL, type, buttons, "%s", msg.c_str());
+	gtk_window_set_title(GTK_WINDOW(dialog), title.c_str());
+	auto result = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
+	if (result == GTK_RESPONSE_OK) {
+		okPressed();
+	} else {
+		cancelPressed();
+	}
 }
 
+void linuxTwoOptionCancelDialog(std::string title,
+								std::string msg,
+								std::string buttonOneText,
+								std::function<void()> buttonOnePressed,
+								std::string buttonTwoText,
+								std::function<void()> buttonTwoPressed,
+								std::function<void()> cancelPressed) {
+	GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
+													nullptr,
+													(GtkDialogFlags) 0,
+													buttonOneText.c_str(),
+													1,
+													buttonTwoText.c_str(),
+													2,
+													"Cancel",
+													3,
 
-void linuxTwoOptionCancelDialog(std::string title, std::string msg,
-                                std::string buttonOneText, std::function<void()> buttonOnePressed,
-                                std::string buttonTwoText, std::function<void()> buttonTwoPressed,
-                                std::function<void()> cancelPressed) {
+													nullptr);
 
-    GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
-                                                    nullptr,
-                                                    (GtkDialogFlags) 0,
-                                                    buttonOneText.c_str(), 1,
-                                                    buttonTwoText.c_str(), 2,
-                                                    "Cancel", 3,
+	GtkWidget *label = gtk_label_new(msg.c_str());
 
-                                                    nullptr);
+	//    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
+	gtk_widget_show_all(dialog);
 
-    GtkWidget *label = gtk_label_new(msg.c_str());
-
-
-//    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
-    gtk_container_add(GTK_CONTAINER (gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
-    gtk_widget_show_all(dialog);
-
-    auto result = gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-    while (g_main_context_iteration(nullptr, false));
-    if (result == 1) {
-        buttonOnePressed();
-    } else if (result == 2) {
-        buttonTwoPressed();
-    } else {
-        cancelPressed();
-    }
+	auto result = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
+	if (result == 1) {
+		buttonOnePressed();
+	} else if (result == 2) {
+		buttonTwoPressed();
+	} else {
+		cancelPressed();
+	}
 }
 
-void linuxTwoOptionDialog(std::string title, std::string msg,
-                                std::string buttonOneText, std::function<void()> buttonOnePressed,
-                                std::string buttonTwoText, std::function<void()> buttonTwoPressed) {
+void linuxTwoOptionDialog(std::string title,
+						  std::string msg,
+						  std::string buttonOneText,
+						  std::function<void()> buttonOnePressed,
+						  std::string buttonTwoText,
+						  std::function<void()> buttonTwoPressed) {
+	GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
+													nullptr,
+													(GtkDialogFlags) 0,
+													buttonOneText.c_str(),
+													1,
+													buttonTwoText.c_str(),
+													2,
 
-    GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
-                                                    nullptr,
-                                                    (GtkDialogFlags) 0,
-                                                    buttonOneText.c_str(), 1,
-                                                    buttonTwoText.c_str(), 2,
+													nullptr);
 
-                                                    nullptr);
+	GtkWidget *label = gtk_label_new(msg.c_str());
 
-    GtkWidget *label = gtk_label_new(msg.c_str());
+	//    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
+	gtk_widget_show_all(dialog);
 
-
-//    gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
-    gtk_container_add(GTK_CONTAINER (gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
-    gtk_widget_show_all(dialog);
-
-    auto result = gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-    while (g_main_context_iteration(nullptr, false));
-    if (result == 1) {
-        buttonOnePressed();
-    } else if (result == 2) {
-        buttonTwoPressed();
-    }
+	auto result = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
+	if (result == 1) {
+		buttonOnePressed();
+	} else if (result == 2) {
+		buttonTwoPressed();
+	}
 }
 
-void linuxThreeOptionCancelDialog(std::string title, std::string msg,
-                                  std::string buttonOneText, std::function<void()> buttonOnePressed,
-                                  std::string buttonTwoText, std::function<void()> buttonTwoPressed,
-                                  std::string buttonThreeText, std::function<void()> buttonThreePressed,
-                                  std::function<void()> cancelPressed) {
+void linuxThreeOptionCancelDialog(std::string title,
+								  std::string msg,
+								  std::string buttonOneText,
+								  std::function<void()> buttonOnePressed,
+								  std::string buttonTwoText,
+								  std::function<void()> buttonTwoPressed,
+								  std::string buttonThreeText,
+								  std::function<void()> buttonThreePressed,
+								  std::function<void()> cancelPressed) {
+	GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
+													nullptr,
+													(GtkDialogFlags) 0,
+													buttonOneText.c_str(),
+													1,
+													buttonTwoText.c_str(),
+													2,
+													buttonThreeText.c_str(),
+													3,
+													"Cancel",
+													4,
 
-    GtkWidget *dialog = gtk_dialog_new_with_buttons(title.c_str(),
-                                                    nullptr,
-                                                    (GtkDialogFlags) 0,
-                                                    buttonOneText.c_str(), 1,
-                                                    buttonTwoText.c_str(), 2,
-                                                    buttonThreeText.c_str(), 3,
-                                                    "Cancel", 4,
+													nullptr);
 
-                                                    nullptr);
+	GtkWidget *label = gtk_label_new(msg.c_str());
+	//   gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
+	gtk_widget_show_all(dialog);
 
-    GtkWidget *label = gtk_label_new(msg.c_str());
-//   gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), label);
-    gtk_container_add(GTK_CONTAINER (gtk_dialog_get_content_area(GTK_DIALOG(dialog))), label);
-    gtk_widget_show_all(dialog);
-
-    auto result = gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-    while (g_main_context_iteration(nullptr, false));
-    if (result == 1) {
-        buttonOnePressed();
-    } else if (result == 2) {
-        buttonTwoPressed();
-    } else if (result == 3) {
-        buttonThreePressed();
-    } else {
-        cancelPressed();
-    }
+	auto result = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	while (g_main_context_iteration(nullptr, false))
+		;
+	if (result == 1) {
+		buttonOnePressed();
+	} else if (result == 2) {
+		buttonTwoPressed();
+	} else if (result == 3) {
+		buttonThreePressed();
+	} else {
+		cancelPressed();
+	}
 }
-
-
