7251f3cce21e3d3cc964ee1087aa6da7c42f0dfa
milo1000 <gieceker@gmail.com>
2023-03-10 18:14:27 +0100

    Win: compilation fix

--- lib/ZipFile/lib/zipper/zipper/tools.cpp
+++ lib/ZipFile/lib/zipper/zipper/tools.cpp
@@ -113,7 +113,8 @@ std::vector<fs::path> filesFromDirectory(const std::string& path)
 
     for (auto& dir_entry : fs::recursive_directory_iterator(fs::u8path(path)))
     {
-		if(!CDirEntry::isDir(dir_entry.path())) {
+        if (!isDirectory(dir_entry.path()))
+        {
 			dir_ls.push_back(dir_entry.path());
 		}
     }
