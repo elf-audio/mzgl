f98d38b4d75b40d2d432d4666f554933b27e1e17
Marek-mba <bereza@gmail.com>
2023-06-30 17:38:50 +0100

    rectf and mitred line tweaks

--- lib/mzgl/geom/Rectf.h
+++ lib/mzgl/geom/Rectf.h
@@ -19,148 +19,180 @@ public:
 	float width = 0;
 	float height = 0;
 	Rectf() {}
-	Rectf(float x, float y, float width, float height) :
-	x(x),
-	y(y),
-	width(width),
-	height(height)
-	{}
-	
+	Rectf(float x, float y, float width, float height)
+		: x(x)
+		, y(y)
+		, width(width)
+		, height(height) {}
+
 	void set(float x, float y, float w, float h);
-	
+
 	void set(const Rectf &r);
-	
-	void translate(glm::vec2 t) { x += t.x; y += t.y; }
-	void translate(float tx, float ty) { x += tx; y += ty; }
-	
+
+	void translate(glm::vec2 t) {
+		x += t.x;
+		y += t.y;
+	}
+	void translate(float tx, float ty) {
+		x += tx;
+		y += ty;
+	}
+
 	bool inside(float x, float y) const;
 	bool inside(glm::vec2 p) const;
 	bool inside(const Rectf &r) const;
 	bool intersects(const Rectf &r) const;
 	void setFromCentre(float cx, float cy, float w, float h);
 	void setFromCentre(glm::vec2 c, float w, float h) { setFromCentre(c.x, c.y, w, h); }
-	
-	static Rectf fromCentre(float cx, float cy, float w, float h) { Rectf r; r.setFromCentre(cx,cy,w,h); return r;}
-	static Rectf fromCentre(glm::vec2 c, float w, float h) { Rectf r; r.setFromCentre(c,w,h); return r;}
+
+	static Rectf fromCentre(float cx, float cy, float w, float h) {
+		Rectf r;
+		r.setFromCentre(cx, cy, w, h);
+		return r;
+	}
+	static Rectf fromCentre(glm::vec2 c, float w, float h) {
+		Rectf r;
+		r.setFromCentre(c, w, h);
+		return r;
+	}
 	glm::vec2 tl() const { return glm::vec2(x, y); }
 	glm::vec2 tr() const { return glm::vec2(x + width, y); }
 	glm::vec2 bl() const { return glm::vec2(x, y + height); }
 	glm::vec2 br() const { return glm::vec2(x + width, y + height); }
 	float bottom() const { return y + height; }
-	float right()  const { return x + width; }
-	glm::vec2 centre() const {
-		return glm::vec2(x + width / 2.f, y + height / 2.f);
-	}
+	float right() const { return x + width; }
+	glm::vec2 centre() const { return glm::vec2(x + width / 2.f, y + height / 2.f); }
 	glm::vec2 size() const { return glm::vec2(width, height); }
-	void size(glm::vec2 s) { width = s.x; height = s.y; }
-	void size(float w, float h) { width = w; height = h; }
-	void size(float d) { width = d; height = d; }
-	
-	float centreX() const { return x + width*0.5f; }
-	float centreY() const { return y + height*0.5f; }
-	
+	void size(glm::vec2 s) {
+		width = s.x;
+		height = s.y;
+	}
+	void size(float w, float h) {
+		width = w;
+		height = h;
+	}
+	void size(float d) {
+		width = d;
+		height = d;
+	}
+
+	float centreX() const { return x + width * 0.5f; }
+	float centreY() const { return y + height * 0.5f; }
+
 	glm::vec2 centreTop() const { return {centreX(), y}; }
 	glm::vec2 centreBottom() const { return {centreX(), bottom()}; }
-	
+
 	glm::vec2 centreLeft() const { return {x, centreY()}; }
 	glm::vec2 centreRight() const { return {right(), centreY()}; }
-	
-	void position(float x, float y) { this->x = x; this->y = y;}
-	
+
+	void position(float x, float y) {
+		this->x = x;
+		this->y = y;
+	}
+
 	glm::vec2 position() const { return glm::vec2(x, y); }
-	void position(glm::vec2 pos) {this->x = pos.x; this->y = pos.y;}
-	
-	
+	void position(glm::vec2 pos) {
+		this->x = pos.x;
+		this->y = pos.y;
+	}
+
 	// Rectf convenience methods
 	void setCentre(float x, float y) { setFromCentre(x, y, this->width, this->height); }
 	void setCentre(const glm::vec2 &c) { setCentre(c.x, c.y); }
-	
-	void setCentreX(float cx) { x = cx - width*0.5f; }
-	void setCentreY(float cy) { x = cy - height*0.5f; }
-	
-	void scale(float amt) {auto c = centre(); setFromCentre(c, width * amt, height * amt); }
-	void scale(float amtX, float amtY) {auto c = centre(); setFromCentre(c, width * amtX, height * amtY); }
-	
+
+	void setCentreX(float cx) { x = cx - width * 0.5f; }
+	void setCentreY(float cy) { x = cy - height * 0.5f; }
+
+	void scale(float amt) {
+		auto c = centre();
+		setFromCentre(c, width * amt, height * amt);
+	}
+	void scale(float amtX, float amtY) {
+		auto c = centre();
+		setFromCentre(c, width * amtX, height * amtY);
+	}
+
 	// repositions the rect so its right or bottom edge is at the argument - doesn't change the box size
-	void setRight(float right) {x = right - width; }
+	void setRight(float right) { x = right - width; }
 	void setBottom(float bottom) { y = bottom - height; }
-	
+
 	// sets left/right without moving the other edge (grows or shrinks the box)
-	void setLeftEdge(float left) { width += x - left; x = left; }
+	void setLeftEdge(float left) {
+		width += x - left;
+		x = left;
+	}
 	void setRightEdge(float right) { width = right - x; }
-	void setTopEdge(float top) { height += y - top; y = top; }
+	void setTopEdge(float top) {
+		height += y - top;
+		y = top;
+	}
 	void setBottomEdge(float bottom) { height = bottom - y; }
-	
+
 	void setTopLeft(float x, float y) { position(x, y); }
 	void setTopLeft(glm::vec2 p) { position(p); }
-	
-	void setBottomLeft(float x, float y) { set(x, y-height, width, height); }
-	void setBottomLeft(glm::vec2 p) { set(p.x, p.y-this->height, this->width, this->height); }
-	
-	void setBottomRight(float x, float y) { set(x - this->width, y-this->height, this->width, this->height); }
-	void setBottomRight(glm::vec2 p) { set(p.x - this->width, p.y-this->height, this->width, this->height); }
-	
+
+	void setBottomLeft(float x, float y) { set(x, y - height, width, height); }
+	void setBottomLeft(glm::vec2 p) { set(p.x, p.y - this->height, this->width, this->height); }
+
+	void setBottomRight(float x, float y) { set(x - this->width, y - this->height, this->width, this->height); }
+	void setBottomRight(glm::vec2 p) { set(p.x - this->width, p.y - this->height, this->width, this->height); }
+
 	void setTopRight(float x, float y) { set(x - this->width, y, this->width, this->height); }
 	void setTopRight(glm::vec2 p) { set(p.x - this->width, p.y, this->width, this->height); }
-	
+
 	// if there's a negative dimension, it positivizes it
 	void normalize() {
-		if(width<0) {
+		if (width < 0) {
 			width = -width;
 			x -= width;
 		}
-		if(height<0) {
+		if (height < 0) {
 			height = -height;
 			y -= height;
 		}
 	}
-	
-	float getMinX() const { return (width  > 0 ? x : (x+width));};
-	float getMinY() const { return (height > 0 ? y : (y+height));};
-	float getMaxX() const { return (width  > 0 ? (x+width)  : x);};
-	float getMaxY() const { return (height > 0 ? (y+height) : y);};
-	
+
+	float getMinX() const { return (width > 0 ? x : (x + width)); };
+	float getMinY() const { return (height > 0 ? y : (y + height)); };
+	float getMaxX() const { return (width > 0 ? (x + width) : x); };
+	float getMaxY() const { return (height > 0 ? (y + height) : y); };
+
 	float getMaxDimension() const { return width > height ? width : height; }
 	float getMinDimension() const { return width < height ? width : height; }
-	
+
 	void growToInclude(glm::vec2 p);
 	void growToInclude(const Rectf &other);
 	void inset(float amt);
 	void inset(glm::vec2 amt);
 	void inset(float horiz, float vert);
 	void inset(float top, float right, float bottom, float left);
-	
+
 	Rectf getIntersection(const Rectf &other) const;
 	void alignToPixels();
-	
-	bool operator==(const Rectf &o) const {
-		return x == o.x && y == o.y && width == o.width && height == o.height;
-	}
-	
-	bool operator!=(const Rectf &o) const {
-		return x != o.x || y != o.y || width != o.width || height != o.height;
-	}
-	
-	Rectf &operator+=(const glm::vec2& offset) {
+
+	glm::vec2 normToPos(glm::vec2 a) const { return {x + a.x * width, y + a.y * height}; }
+	bool operator==(const Rectf &o) const { return x == o.x && y == o.y && width == o.width && height == o.height; }
+
+	bool operator!=(const Rectf &o) const { return x != o.x || y != o.y || width != o.width || height != o.height; }
+
+	Rectf &operator+=(const glm::vec2 &offset) {
 		x += offset.x;
 		y += offset.y;
 		return *this;
 	}
-	
-	Rectf &operator-=(const glm::vec2& offset) {
+
+	Rectf &operator-=(const glm::vec2 &offset) {
 		x -= offset.x;
 		y -= offset.y;
 		return *this;
 	}
-	
-	Rectf& operator*=(const float& rhs){
-		
+
+	Rectf &operator*=(const float &rhs) {
 		x *= rhs;
 		y *= rhs;
 		width *= rhs;
 		height *= rhs;
-		
+
 		return *this;
 	}
-		
 };
--- lib/mzgl/gl/drawing/MitredLine.cpp
+++ lib/mzgl/gl/drawing/MitredLine.cpp
@@ -12,25 +12,25 @@ using namespace std;
 void MitredLine::getStripCoord(const glm::vec2 &before, const glm::vec2 &curr, const glm::vec2 &after, glm::vec2 &a, glm::vec2 &b) {
 	glm::vec2 n1 = getNormal(curr - before);
 	glm::vec2 n2 = getNormal(after - curr);
-	
-	if(outside) {
+
+	if (outside) {
 		top1.setFrom2Points(before, curr);
 		top2.setFrom2Points(curr, after);
-		
-		bottom1.setFrom2Points(before - n1*2.f, curr - n1*2.f);
-		bottom2.setFrom2Points(curr - n2*2.f, after - n2*2.f);
+
+		bottom1.setFrom2Points(before - n1 * 2.f, curr - n1 * 2.f);
+		bottom2.setFrom2Points(curr - n2 * 2.f, after - n2 * 2.f);
 	} else {
 		top1.setFrom2Points(before - n1, curr - n1);
 		top2.setFrom2Points(curr - n2, after - n2);
-		
+
 		bottom1.setFrom2Points(before + n1, curr + n1);
 		bottom2.setFrom2Points(curr + n2, after + n2);
 	}
-	if(abs(top1.m-top2.m)<0.0001) {
+	if (abs(top1.m - top2.m) < 0.0001) {
 		// parallel, so don't try to find intersection
-		if(outside) {
+		if (outside) {
 			a = (curr);
-			b = (curr - n1*2.f);
+			b = (curr - n1 * 2.f);
 		} else {
 			a = (curr - n1);
 			b = (curr + n1);
@@ -38,19 +38,16 @@ void MitredLine::getStripCoord(const glm::vec2 &before, const glm::vec2 &curr, c
 	} else {
 		a = top1.getIntersection(top2);
 		b = bottom1.getIntersection(bottom2);
-		
 	}
-//	return true;
+	//	return true;
 }
 
-
 int MitredLine::getVerts(const vector<glm::vec2> &p, vector<glm::vec2> &outVerts, vector<unsigned int> &indices, bool close, bool bevelled) {
-	
-	int originalVertCount = (int)outVerts.size();
-	
-	
+	int originalVertCount = (int) outVerts.size();
+
+	if (p.size() < 2) return 0;
 	vector<glm::vec2> strip;
-	if(close) {
+	if (close) {
 		glm::vec2 a;
 		glm::vec2 b;
 		getStripCoord(p.back(), p[0], p[1], a, b);
@@ -58,7 +55,7 @@ int MitredLine::getVerts(const vector<glm::vec2> &p, vector<glm::vec2> &outVerts
 		strip.push_back(b);
 	} else {
 		glm::vec2 n = getNormal(p[1] - p[0]);
-		if(squareCap) {
+		if (squareCap) {
 			glm::vec2 cap(n.y, -n.x);
 			strip.push_back(p[0] - n - cap);
 			strip.push_back(p[0] + n - cap);
@@ -66,34 +63,31 @@ int MitredLine::getVerts(const vector<glm::vec2> &p, vector<glm::vec2> &outVerts
 			strip.push_back(p[0] - n);
 			strip.push_back(p[0] + n);
 		}
-		
 	}
-	
-	
-	
-	if(bevelled) {
-		for(int i = 1; i < p.size()-1; i++) {
-			auto n = getNormal(p[i] - p[i-1]);
+
+	if (bevelled) {
+		for (int i = 1; i < p.size() - 1; i++) {
+			auto n = getNormal(p[i] - p[i - 1]);
 			strip.emplace_back(p[i] - n);
 			strip.emplace_back(p[i] + n);
-			
-			n = getNormal(p[i+1] - p[i]);
+
+			n = getNormal(p[i + 1] - p[i]);
 			strip.emplace_back(p[i] - n);
 			strip.emplace_back(p[i] + n);
 		}
 	} else {
-		for(int i = 1; i < p.size()-1; i++) {
+		for (int i = 1; i < p.size() - 1; i++) {
 			glm::vec2 a;
 			glm::vec2 b;
-			getStripCoord(p[i-1], p[i], p[i+1], a, b);
-			if(mitreLimit) {
-				if(glm::distance(p[i], a)>thickness*0.5) {
-					auto da = glm::normalize(a - p[i]) * thickness*0.5f;
+			getStripCoord(p[i - 1], p[i], p[i + 1], a, b);
+			if (mitreLimit) {
+				if (glm::distance(p[i], a) > thickness * 0.5) {
+					auto da = glm::normalize(a - p[i]) * thickness * 0.5f;
 					a = p[i] + da;
 				}
-				
-				if(glm::distance(p[i], b)>thickness*0.5) {
-					auto db = glm::normalize(b - p[i]) * thickness*0.5f;
+
+				if (glm::distance(p[i], b) > thickness * 0.5) {
+					auto db = glm::normalize(b - p[i]) * thickness * 0.5f;
 					b = p[i] + db;
 				}
 			}
@@ -101,45 +95,43 @@ int MitredLine::getVerts(const vector<glm::vec2> &p, vector<glm::vec2> &outVerts
 			strip.emplace_back(b);
 		}
 	}
-	
-	if(close) {
-		
+
+	if (close) {
 		glm::vec2 a;
 		glm::vec2 b;
-		getStripCoord(p[p.size()-2], p.back(), p[0], a, b);
+		getStripCoord(p[p.size() - 2], p.back(), p[0], a, b);
 		strip.push_back(a);
 		strip.push_back(b);
-		
+
 		strip.push_back(strip[0]);
 		strip.push_back(strip[1]);
-		
+
 	} else {
-		glm::vec2 n = getNormal(p[p.size()-1] - p[p.size()-2]);
-		if(squareCap) {
+		glm::vec2 n = getNormal(p[p.size() - 1] - p[p.size() - 2]);
+		if (squareCap) {
 			glm::vec2 cap(n.y, -n.x);
-			strip.push_back(p[p.size()-1] - n + cap);
-			strip.push_back(p[p.size()-1] + n + cap);
+			strip.push_back(p[p.size() - 1] - n + cap);
+			strip.push_back(p[p.size() - 1] + n + cap);
 		} else {
-			strip.push_back(p[p.size()-1] - n);
-			strip.push_back(p[p.size()-1] + n);
+			strip.push_back(p[p.size() - 1] - n);
+			strip.push_back(p[p.size() - 1] + n);
 		}
 	}
-	
-	
-	int startIndex = (int)outVerts.size();
+
+	int startIndex = (int) outVerts.size();
 	outVerts.insert(outVerts.end(), strip.begin(), strip.end());
-	indices.reserve(indices.size() + strip.size()*3);
-	for(int i = 2; i < strip.size(); i+=2) {
+	indices.reserve(indices.size() + strip.size() * 3);
+	for (int i = 2; i < strip.size(); i += 2) {
 		indices.push_back(startIndex + i - 2);
 		indices.push_back(startIndex + i);
 		indices.push_back(startIndex + i + 1);
-		
+
 		indices.push_back(startIndex + i + 1);
 		indices.push_back(startIndex + i - 1);
 		indices.push_back(startIndex + i - 2);
 	}
-	
-	return (int)outVerts.size() - originalVertCount;
+
+	return (int) outVerts.size() - originalVertCount;
 }
 
 int MitredLine::getVertsBevelled(const std::vector<glm::vec2> &p, std::vector<glm::vec2> &outVerts, std::vector<unsigned int> &indices, bool close) {
