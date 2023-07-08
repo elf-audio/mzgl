c897785747af035ecb7429d9bb62f063811e2b9e
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-07-08 19:36:51 +0100

    Add DesktopWindowEventHandler

---
+++ lib/mzgl/app/glfw/DesktopWindowEventHandler.cpp
@@ -0,0 +1,102 @@
+#include "DesktopWindowEventHandler.h"
+#include "EventDispatcher.h"
+
+auto DesktopWindowEventHandler::isAnyMouseButtonDown() const -> bool {
+    for (const auto b : buttons_) {
+        if (b) {
+            return true;
+		}
+	}
+    return false;
+}
+
+auto DesktopWindowEventHandler::isDown(Modifier modifier) const -> bool {
+	if (modifier == Modifier::none) {
+		return false;
+	}
+	return modifiers_[size_t(modifier)];
+}
+
+auto DesktopWindowEventHandler::cursorPos(EventDispatcher* eventDispatcher, float x, float y) -> void {
+    mouseX_ = x;
+    mouseY_ = y;
+    if (!isAnyMouseButtonDown()) {
+		eventDispatcher->touchOver(mouseX_, mouseY_);
+		return;
+	}
+	for(int i = 0; i < buttons_.size(); i++) {
+		if(buttons_[i]) {
+			eventDispatcher->touchMoved(mouseX_, mouseY_, i);
+		}
+	}
+}
+
+auto DesktopWindowEventHandler::filesDropped(EventDispatcher* eventDispatcher, std::vector<std::string> paths) -> void {
+	// FIXME: This isn't currently good enough
+	// See https://github.com/elf-audio/koala/issues/117
+	eventDispatcher->filesDropped(std::move(paths), -1, []{});
+}
+
+auto DesktopWindowEventHandler::getModifier(int key) const -> Modifier {
+	switch (key) {
+		case GLFW_KEY_LEFT_ALT: return Modifier::left_alt;
+		case GLFW_KEY_RIGHT_ALT: return Modifier ::right_alt;
+		case GLFW_KEY_LEFT_SHIFT: return Modifier::left_shift;
+		case GLFW_KEY_RIGHT_SHIFT: return Modifier ::right_shift;
+	}
+	return Modifier::none;
+}
+
+auto DesktopWindowEventHandler::key(EventDispatcher* eventDispatcher, int key, int action) -> void {
+    if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+		keyDown(eventDispatcher, key);
+		return;
+    }
+    if (action == GLFW_RELEASE) {
+		keyUp(eventDispatcher, key);
+		return;
+    }
+}
+
+auto DesktopWindowEventHandler::keyDown(EventDispatcher* eventDispatcher, int key) -> void {
+	const auto modifier{getModifier(key)};
+	eventDispatcher->keyDown(key);
+	if (modifier != Modifier::none) {
+		modifiers_[size_t(modifier)] = true;
+	}
+}
+
+auto DesktopWindowEventHandler::keyUp(EventDispatcher* eventDispatcher, int key) -> void {
+	const auto modifier{getModifier(key)};
+	eventDispatcher->keyUp(key);
+	if (modifier != Modifier::none) {
+		modifiers_[size_t(modifier)] = false;
+	}
+}
+
+auto DesktopWindowEventHandler::mouseButton(EventDispatcher* eventDispatcher, int button, int action) -> void {
+    if (action == GLFW_PRESS) {
+        buttons_[button] = true;
+        eventDispatcher->touchDown(mouseX_, mouseY_, button);
+        return;
+	}
+	eventDispatcher->touchUp(mouseX_, mouseY_, button);
+	buttons_[button] = false;
+}
+
+auto DesktopWindowEventHandler::scroll(EventDispatcher* eventDispatcher, float x, float y) -> void {
+	if (isDown(Modifier::left_alt) || isDown(Modifier::right_alt)) {
+        eventDispatcher->mouseZoomed(mouseX_, mouseY_, y * -0.03);
+		return;
+    }
+#ifdef WIN32
+	// speed up windows scrolling.
+	// should really have acceleration here...
+	x *= 3;
+	y *= 3;
+#endif
+	if (isDown(Modifier::left_shift) || isDown(Modifier::right_shift)) {
+		std::swap(x, y);
+	}
+	eventDispatcher->mouseScrolled(mouseX_, mouseY_, x, y);
+}
---
+++ lib/mzgl/app/glfw/DesktopWindowEventHandler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include <vector>
+#include <glfw/glfw3.h>
+
+class EventDispatcher;
+
+struct DesktopWindowEventHandler {
+	auto cursorPos(EventDispatcher* eventDispatcher, float x, float y) -> void;
+	auto filesDropped(EventDispatcher* eventDispatcher, std::vector<std::string> paths) -> void;
+	auto key(EventDispatcher* eventDispatcher, int key, int action) -> void;
+	auto keyDown(EventDispatcher* eventDispatcher, int key) -> void;
+	auto keyUp(EventDispatcher* eventDispatcher, int key) -> void;
+	auto mouseButton(EventDispatcher* eventDispatcher, int button, int action) -> void;
+	auto scroll(EventDispatcher* eventDispatcher, float x, float y) -> void;
+private:
+	enum class Modifier { none = -1, left_alt, right_alt, left_shift, right_shift };
+	auto getModifier(int key) const -> Modifier;
+	auto isAnyMouseButtonDown() const -> bool;
+	auto isDown(Modifier modifier) const -> bool;
+	float mouseX_{0};
+	float mouseY_{0};
+	std::array<bool, 8> buttons_{false};
+	std::array<bool, 4> modifiers_{false};
+};
