ffecfd7aeba4d109de06c78e476388498192c721
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-07-11 13:58:03 +0100

    Unify VST/standalone GLFW stuff more.

---
+++ lib/mzgl/app/glfw/OS_Linux.cpp
@@ -0,0 +1,31 @@
+#include "OS.h"
+#include <glew.h>
+#include "glfw3.h"
+#include <glfw/glfw3native.h>
+
+namespace os {
+
+auto getNativeWindowHandle(GLFWwindow* window) -> void* {
+	return glfwGetGLXWindow(window);
+}
+
+auto initializeGLContext() -> void {
+	// Linux doesn't need to do anything specific right now?
+}
+
+/*
+
+If we build a VST version for Linux then these will need to be implemented:
+
+auto setWindowSize(GLFWwindow* window, int width, int height) -> void {
+}
+
+auto attachWindow(GLFWwindow* window, void* nativeParentWindowHandle) -> void {
+}
+
+auto detachWindow(GLFWwindow* window) -> void {
+}
+
+*/
+
+} // os
