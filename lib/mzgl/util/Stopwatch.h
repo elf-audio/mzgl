deb4ea97107c41c2837346e07508e8294e154a3b
Marek Bereza <bereza@gmail.com>
2023-08-24 18:06:14 +0100

    tidied up stopwatch comments

--- lib/mzgl/util/Stopwatch.h
+++ lib/mzgl/util/Stopwatch.h
@@ -1,50 +1,40 @@
 //
 //  Timer.h
-//  mzgl
-//
-//  Helpful debugging and performance thing.
 //
 //  Created by Marek Bereza on 10/08/2022.
 //  Copyright © 2022 Marek Bereza. All rights reserved.
 //
 
 #pragma once
+
 #include <chrono>
 #include "log.h"
 struct Stopwatch {
+	std::chrono::steady_clock::time_point startTime;
 
-    std::chrono::steady_clock::time_point startTime;
+	/**
+     * @brief Starts Stopwatch.
+     */
+	void start() { startTime = std::chrono::steady_clock::now(); }
 
-    //------------------------------------------------------------------------------------------------------------------------------
-    ///
-    /// @brief Starts Stopwatch.
-    ///
-    //------------------------------------------------------------------------------------------------------------------------------
-    void start() {
-        startTime = std::chrono::steady_clock::now();
-    }
+	/**
+     * @brief Returns the time delta from the start.
+     *
+     * @return time delta in milliseconds
+     */
+	std::uint64_t getTimeDeltaMs() {
+		auto duration = std::chrono::steady_clock::now() - startTime;
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+		return ms.count();
+	}
 
-    //------------------------------------------------------------------------------------------------------------------------------
-    ///
-    /// @brief Returns the time delta from the start.
-    ///
-    /// @return time delta in milliseconds
-    //------------------------------------------------------------------------------------------------------------------------------
-    std::uint64_t getTimeDeltaMs() {
-        auto duration = std::chrono::steady_clock::now() - startTime;
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-        return ms.count();
-    }
-
-    //------------------------------------------------------------------------------------------------------------------------------
-    ///
-    /// @brief Prints the time delta measured from the start.
-    ///
-    /// @param message Message to be printed along with the measured time delta.
-    //------------------------------------------------------------------------------------------------------------------------------
-    void printTimeDeltaMs(const std::string &message) {
-        auto ms = getTimeDeltaMs();
-        Log::d() << message << " took " << ms << "ms";
-//        printf("%s took %lu ms\n", message.c_str(), ms);
-    }
+	/**
+     * @brief Prints the time delta measured from the start.
+     *
+     * @param message Message to be printed along with the measured time delta.
+     */
+	void printTimeDeltaMs(const std::string &message) {
+		auto ms = getTimeDeltaMs();
+		Log::d() << message << " took " << ms << "ms";
+	}
 };
