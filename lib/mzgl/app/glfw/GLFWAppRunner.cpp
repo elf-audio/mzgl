1b20289c6b9070cb39d73b7eb337233019f1c3b4
Marek Bereza <bereza@gmail.com>
2023-09-05 13:44:56 +0100

    logging

--- lib/mzgl/app/glfw/GLFWAppRunner.cpp
+++ lib/mzgl/app/glfw/GLFWAppRunner.cpp
@@ -48,7 +48,8 @@ Graphics &getGraphics(GLFWwindow *window) {
 }
 
 static void error_callback(int error, const char *description) {
-	fprintf(stderr, "Error: %s\n", description);
+	// fprintf(stderr, "Error: %s\n", description);
+	Log::e() << "GLFW Error: " << description;
 }
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
@@ -86,7 +87,7 @@ float getMainMonitorScale() {
 		Log::d() << "scale x: " << xscale << " scale y: " << yscale;
 		return xscale;
 	} else {
-		printf("Error, can't find main monitor\n");
+		Log::d() << "Can't find main monitor";
 	}
 	return 1;
 }
