bc4d5177f44b718529bb71afddee468572172c0b
Marek Bereza <bereza@gmail.com>
2023-10-02 17:55:47 +0100

    more scale utils and a fix to scroller

--- lib/mzgl/music/scales.cpp
+++ lib/mzgl/music/scales.cpp
@@ -8,38 +8,43 @@
 #include <math.h>
 #include <vector>
 
-using namespace std;
+static std::vector<std::string> noteNames = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-
-// this is for C3 = 60 - which is the ableton convention
-string midiNoteNumToString(int note) {
-    if(note<0) return "n/a";
-    static vector<string> noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-    
-    int octave = (note / 12) - 2;
-    int index = note % 12;
-    return noteNames[index] + to_string(octave);
+std::string noteNumToName(int note) {
+	if (note < 0 || note > 11) return "n/a";
+	return noteNames[note];
 }
 
+int noteNameToNum(const std::string &noteName) {
+	for (int i = 0; i < noteNames.size(); i++) {
+		if (noteNames[i] == noteName) return i;
+	}
+	return -1;
+}
 
-float mtof(float f)
-{
+// this is for C3 = 60 - which is the ableton convention
+std::string midiNoteNumToString(int note) {
+	if (note < 0) return "n/a";
+
+	int octave = (note / 12) - 2;
+	int index = note % 12;
+	return noteNumToName(index) + std::to_string(octave);
+}
+
+float mtof(float f) {
 	return (8.17579891564 * exp(.0577622650 * f));
 }
 
-float ftom(float f)
-{
+float ftom(float f) {
 	return (17.3123405046 * log(.12231220585 * f));
 }
 
-
 int getScaled(int pos, int scale) {
-	
-	if(scale==CHROMATIC) return pos;
-	else if(scale==PENTATONIC) {
+	if (scale == CHROMATIC) return pos;
+	else if (scale == PENTATONIC) {
 		int octave = pos / 5;
 		int note = 0;
-		switch(pos % 5) {
+		switch (pos % 5) {
 			case 0: note = 0; break;
 			case 1: note = 3; break;
 			case 2: note = 5; break;
@@ -48,63 +53,60 @@ int getScaled(int pos, int scale) {
 		}
 		return octave * 12 + note;
 	}
-	
-	else if(scale==MINOR) {
+
+	else if (scale == MINOR) {
 		int octave = pos / 7;
 		int note = 0;
-		switch(pos % 7) {
-			case 0: note = 0;  break;
-			case 1: note = 2;  break;
-			case 2: note = 3;  break;
-			case 3: note = 5;  break;
-			case 4: note = 7;  break;
-			case 5: note = 8;  break;
+		switch (pos % 7) {
+			case 0: note = 0; break;
+			case 1: note = 2; break;
+			case 2: note = 3; break;
+			case 3: note = 5; break;
+			case 4: note = 7; break;
+			case 5: note = 8; break;
 			case 6: note = 11; break;
 		}
 		return octave * 12 + note;
 	}
-	
-	else if(scale==MAJOR) {
+
+	else if (scale == MAJOR) {
 		int octave = pos / 7;
 		int note = 0;
-		switch(pos % 7) {
-			case 0: note = 0;  break;
-			case 1: note = 2;  break;
-			case 2: note = 4;  break;
-			case 3: note = 5;  break;
-			case 4: note = 7;  break;
-			case 5: note = 9;  break;
+		switch (pos % 7) {
+			case 0: note = 0; break;
+			case 1: note = 2; break;
+			case 2: note = 4; break;
+			case 3: note = 5; break;
+			case 4: note = 7; break;
+			case 5: note = 9; break;
 			case 6: note = 11; break;
 		}
 		return octave * 12 + note;
 	}
-	
-	else if(scale==WHOLE) {
+
+	else if (scale == WHOLE) {
 		int octave = pos / 6;
 		int note = 0;
-		switch(pos % 6) {
-			case 0: note = 0;  break;
-			case 1: note = 2;  break;
-			case 2: note = 4;  break;
-			case 3: note = 6;  break;
-			case 4: note = 8;  break;
-			case 5: note = 10;  break;
+		switch (pos % 6) {
+			case 0: note = 0; break;
+			case 1: note = 2; break;
+			case 2: note = 4; break;
+			case 3: note = 6; break;
+			case 4: note = 8; break;
+			case 5: note = 10; break;
 		}
 		return octave * 12 + note;
 	}
 #pragma warning not sure what this does
 	return pos;
-	
 }
 
 float midiNoteToSpeed(int midiNote, int originalNote) {
-	return pow(2, (midiNote-originalNote)/12.f);
-	
+	return pow(2, (midiNote - originalNote) / 12.f);
 }
 
 int qwertyToMidi(int k) {
-	switch(k) {
-			
+	switch (k) {
 		case 'a': return 0;
 		case 'w': return 1;
 		case 's': return 2;
@@ -123,20 +125,18 @@ int qwertyToMidi(int k) {
 		case 'p': return 15;
 		case ';': return 16;
 		case '\'': return 17;
-		
-		default : return -1;
+
+		default: return -1;
 	}
 }
 
-
 bool isSharp(int i) {
-	switch((i+144)%12) {
+	switch ((i + 144) % 12) {
 		case 1:
 		case 3:
 		case 6:
 		case 8:
-		case 10:
-			return true;
+		case 10: return true;
 		default: return false;
 	}
 }
--- lib/mzgl/ui/Scroller.cpp
+++ lib/mzgl/ui/Scroller.cpp
@@ -1,9 +1,8 @@
 
 #include "Scroller.h"
 
-
-
-Scroller::Scroller(Graphics &g): Layer(g) {
+Scroller::Scroller(Graphics &g)
+	: Layer(g) {
 	interactive = true;
 	content = new Layer(g, "content");
 	contentHeightExplicitlySet = false;
@@ -12,15 +11,22 @@ Scroller::Scroller(Graphics &g): Layer(g) {
 	color.a = 0.f;
 }
 void Scroller::contentUpdated() {
-	if(!contentHeightExplicitlySet) {
-		if(content->getNumChildren()>0) {
-			content->height = content->getChild(content->getNumChildren()-1)->bottom();
+	if (!contentHeightExplicitlySet) {
+		Layer *lastVisibleChild = nullptr;
+		for (int i = 0; i < content->getNumChildren(); i++) {
+			Layer *c = content->getChild(i);
+			if (c->visible) {
+				lastVisibleChild = c;
+			}
+		}
+		if (lastVisibleChild != nullptr) {
+			content->height = lastVisibleChild->bottom();
 		}
 	}
 }
 void Scroller::addContent(Layer *layer) {
 	content->addChild(layer);
-	if(!contentHeightExplicitlySet) {
+	if (!contentHeightExplicitlySet) {
 		content->height = fmaxf(content->height, layer->bottom());
 	}
 }
@@ -29,84 +35,79 @@ void Scroller::clear() {
 	content->height = 0;
 }
 void Scroller::update() {
-	if(!scrolling) {
+	if (!scrolling) {
 		float lerpSpeed = 0.86;
-		if(content->y>0) {
+		if (content->y > 0) {
 			content->y *= lerpSpeed;
-		} else if(content->height < height) {//} && content->y < 0) {
+		} else if (content->height < height) { //} && content->y < 0) {
 			content->y *= lerpSpeed;
-		//} else if(content->height < height && content->y > 0) {
-			
-		} else if(content->bottom()<height) {
-			content->y = content->y * lerpSpeed + (height - content->height)*(1.0 - lerpSpeed);
+			//} else if(content->height < height && content->y > 0) {
+
+		} else if (content->bottom() < height) {
+			content->y = content->y * lerpSpeed + (height - content->height) * (1.0 - lerpSpeed);
 		}
-		
+
 		contentVelocity *= 0.9;
 		content->y += contentVelocity.y;
-		if(abs(contentVelocity.y)<0.01) {
+		if (abs(contentVelocity.y) < 0.01) {
 			scrollbarActivityAmt -= 0.1;
-			if(scrollbarActivityAmt<0) scrollbarActivityAmt = 0;
+			if (scrollbarActivityAmt < 0) scrollbarActivityAmt = 0;
 		}
 	} else {
 		scrollbarActivityAmt += 0.1;
-		if(scrollbarActivityAmt>1) scrollbarActivityAmt = 1;
+		if (scrollbarActivityAmt > 1) scrollbarActivityAmt = 1;
 	}
 }
 
 void Scroller::draw() {
-	if(color.a>0) {
+	if (color.a > 0) {
 		g.setColor(color);
 		g.drawRect(*this);
 	}
 }
 
 void Scroller::_draw() {
-	if(!visible) return;
+	if (!visible) return;
 	Layer::_draw();
-	if(drawingScrollbar) {
+	if (drawingScrollbar) {
 		drawScrollbar();
 	}
 }
 void Scroller::drawScrollbar() {
-
-	if(content->height>height) {
+	if (content->height > height) {
 		g.fill();
 		ScopedAlphaBlend scpA(g, true);
-		if(scrollbarDims) {
+		if (scrollbarDims) {
 			g.setColor(1, 1, 1, 0.1 + 0.15 * scrollbarActivityAmt);
 		} else {
 			g.setColor(1, 1, 1, 0.25);
 		}
-		
-	
+
 		float a = content->y / (height - content->height);
 		//mapf(content->y, 0, height - content->height, 0, 1);
 		float squash = 1;
-		if(a>1) {
+		if (a > 1) {
 			squash = 2 - a;
 			a = 1;
-		} else if(a<0) {
+		} else if (a < 0) {
 			squash = 1 + a;
 			a = 0;
 		}
-		
-		if(squash<1) {
-			squash = 1 + tanh(squash - 1) *0.5f;
+
+		if (squash < 1) {
+			squash = 1 + tanh(squash - 1) * 0.5f;
 		}
 
 		float w = 10;
 		float padding = w;
-		
+
 		float h = (height - padding * 2) * (height / content->height) * squash;
-		float yy = y + padding + (height - (padding * 2) - h)*a;
-		g.drawRoundedRect(Rectf(x + width - padding * 2, yy, w, h), w/2);
+		float yy = y + padding + (height - (padding * 2) - h) * a;
+		g.drawRoundedRect(Rectf(x + width - padding * 2, yy, w, h), w / 2);
 	}
 }
 
-
-
 void Scroller::touchOver(float x, float y) {
-
 }
 
 void Scroller::touchUp(float x, float y, int id) {
@@ -114,29 +115,27 @@ void Scroller::touchUp(float x, float y, int id) {
 }
 
 void Scroller::touchMoved(float x, float y, int id) {
-	if(content->height<=height) return;
-	if(scrolling) {
+	if (content->height <= height) return;
+	if (scrolling) {
 		glm::vec2 pos(x, y);
 		glm::vec2 delta = pos - lastTouch;
 
-		
 		contentVelocity = delta;
-		
+
 		// make it hard to drag left and right
-		if(content->y>0) {
-			contentVelocity *= (1 - content->y/height) * 0.5;
-		} else if(content->bottom()<height) {
-			contentVelocity *= (1 - (height - content->bottom())/height) * 0.5;
+		if (content->y > 0) {
+			contentVelocity *= (1 - content->y / height) * 0.5;
+		} else if (content->bottom() < height) {
+			contentVelocity *= (1 - (height - content->bottom()) / height) * 0.5;
 		}
 		content->y += contentVelocity.y;
 
-		
 		lastTouch = pos;
 	}
 }
 
 bool Scroller::touchDown(float x, float y, int id) {
-	if(inside(x, y)) {
+	if (inside(x, y)) {
 		lastTouch = glm::vec2(x, y);
 		scrolling = true;
 		return true;
@@ -149,11 +148,10 @@ void Scroller::setContentHeight(float contentHeight) {
 	this->content->height = contentHeight;
 	contentHeightExplicitlySet = true;
 }
-void Scroller::mouseScrolled(float x, float y, float scrollX, float scrollY ) {
-	if(content->height<=height) {
+void Scroller::mouseScrolled(float x, float y, float scrollX, float scrollY) {
+	if (content->height <= height) {
 		return;
 	}
 
-	contentVelocity = vec2(scrollX*4.f, scrollY*4.f);
-
+	contentVelocity = vec2(scrollX * 4.f, scrollY * 4.f);
 }
