6e01eb699f4fc2f5a64ec37312e353504dd56467
Marek-mba <bereza@gmail.com>
2023-11-11 17:19:36 +0800

    got rid of clampf

--- lib/mzgl/ui/Knob.h
+++ lib/mzgl/ui/Knob.h
@@ -17,98 +17,88 @@ public:
 	float max;
 	bool enabled = true;
 	bool down = false;
-	
-	
+
 	string niceToString(float val) {
-		if(val<2) {
+		if (val < 2) {
 			return to_string(val, 2);
-		} else if(val<50) {
+		} else if (val < 50) {
 			return to_string(val, 1);
 		} else {
-			return to_string((int)val);
+			return to_string((int) val);
 		}
 	}
-	
-	
-	Knob(Graphics &g, string name, float &value, float min = 0, float max = 1) :
-	Layer(g),
-	name(name),
-	value(value),
-	min(min),
-	max(max)
-	{
+
+	Knob(Graphics &g, string name, float &value, float min = 0, float max = 1)
+		: Layer(g)
+		, name(name)
+		, value(value)
+		, min(min)
+		, max(max) {
 		interactive = true;
 		width = 100;
 		height = 100;
 	}
-	
+
 	void draw() override {
 		vec2 c = centre();
 		g.setColor(1);
-		if(!enabled) g.noFill();
-		else g.fill();
-		
-		g.drawCircle(c, width/2);
-		
-		
-		float angle = mapf(value, min, max, -225,  45);
-		
+		if (!enabled) g.noFill();
+		else
+			g.fill();
+
+		g.drawCircle(c, width / 2);
+
+		float angle = mapf(value, min, max, -225, 45);
+
 		float theta = angle * M_PI / 180.f;
-		
+
 		g.pushMatrix();
 		g.translate(c.x, c.y);
 		g.rotateZ(theta);
-		
-		
-//		vec2 ang(cos(theta), sin(theta));
-//		vec2 a = c + ang * width * 0.5f;
-//		vec2 b = c + ang * 0.7f * width * 0.5f;
+
+		//		vec2 ang(cos(theta), sin(theta));
+		//		vec2 a = c + ang * width * 0.5f;
+		//		vec2 b = c + ang * 0.7f * width * 0.5f;
 		g.setColor(0);
-//		g.drawLine(a, b);
-		g.drawRect(width/4, -2, width/4, 4);
-		
+		//		g.drawLine(a, b);
+		g.drawRect(width / 4, -2, width / 4, 4);
+
 		g.popMatrix();
 		g.setColor(1);
-		g.drawTextCentred(name, {x + width/2, y + height + 20});
-		if(down) {
+		g.drawTextCentred(name, {x + width / 2, y + height + 20});
+		if (down) {
 			g.setColor(0);
 			g.drawTextCentred(niceToString(value), c);
 		}
 	}
-	
+
 	bool rounding = false;
-	void setRounding(bool rounding) {
-		this->rounding = rounding;
-	}
-	
+	void setRounding(bool rounding) { this->rounding = rounding; }
+
 	bool touchDown(float x, float y, int id) override {
-		if(!enabled) return false;
+		if (!enabled) return false;
 		down = true;
-	
+
 		startValue = value;
 		startTouchY = y;
 		currYTouch = y;
-		
+
 		return true;
 	}
 	float currYTouch = 0;
 	float startTouchY = 0;
 	float startValue = 0;
-	
+
 	void touchMoved(float x, float y, int id) override {
-		
-		if(!enabled) return;
-		
+		if (!enabled) return;
+
 		currYTouch = y;
 		float gain = 0.0025;
-		value = startValue -(y - startTouchY) * gain * (max - min);
-		
-		value = clampf(value, min,max);
-		if(rounding) value = round(value);
-		if(value==-0.f) value = 0.f;
-	}
-	void touchUp(float x, float y, int id) override {
-		down = false;
-	}
-};
+		value = startValue - (y - startTouchY) * gain * (max - min);
 
+		value = std::clamp(value, min, max);
+		if (rounding) value = round(value);
+		if (value == -0.f) value = 0.f;
+	}
+	void touchUp(float x, float y, int id) override { down = false; }
+};
--- lib/mzgl/ui/ScrollingListDeletable.cpp
+++ lib/mzgl/ui/ScrollingListDeletable.cpp
@@ -15,32 +15,31 @@
 #include <algorithm>
 
 void ScrollingListDeletableView::draw() {
-	
-	if(collapsing) {
+	if (collapsing) {
 		return;
 	}
 	g.setColor(settings.buttonTextColor);
-	float notDeletePos = x + width + std::max(-settings.deleteButtonWidth/2.f, horizontalScroll + settings.deleteButtonWidth/2.f);
-	float deletePos = x + width + std::max(horizontalScroll, -width) + settings.deleteButtonWidth/2.f;
-	
+	float notDeletePos = x + width + std::max(-settings.deleteButtonWidth / 2.f, horizontalScroll + settings.deleteButtonWidth / 2.f);
+	float deletePos = x + width + std::max(horizontalScroll, -width) + settings.deleteButtonWidth / 2.f;
+
 	float pos = notDeletePos;
-	if(deleting) {
-		deleteDecidey = clampf(deleteDecidey + (shouldDelete?0.05:-0.05), 0, 1);
+	if (deleting) {
+		deleteDecidey = std::clamp(deleteDecidey + (shouldDelete ? 0.05f : -0.05f), 0.f, 1.f);
 		pos = mapf(easeInOutCubic(deleteDecidey), 0, 1, notDeletePos, deletePos);
 	}
 
-	if(horizontalScroll<0) {
-		if(settings.font==nullptr) {
+	if (horizontalScroll < 0) {
+		if (settings.font == nullptr) {
 			g.drawTextCentred("DELETE", vec2(pos, y + height / 2));
 		} else {
 			settings.font->drawCentred(g, "DELETE", vec2(pos, y + height / 2));
 		}
 	}
-	
-	if(settings.hasActionButton) {
-		auto p = vec2(horizontalScroll - settings.actionButtonWidth/2, y + height / 2);
-		
-		if(settings.font==nullptr) {
+
+	if (settings.hasActionButton) {
+		auto p = vec2(horizontalScroll - settings.actionButtonWidth / 2, y + height / 2);
+
+		if (settings.font == nullptr) {
 			g.drawTextCentred(settings.actionName, p);
 		} else {
 			settings.font->drawCentred(g, settings.actionName, p);
@@ -49,64 +48,59 @@ void ScrollingListDeletableView::draw() {
 }
 
 void ScrollingListDeletableView::draw(Drawer &d) {
-	
-//	if(down && length(totalMovement)<10) {
-//		selected = true;
-//	}
-	if(down && length(totalMovement)<10 && downCount>=3) {
+	//	if(down && length(totalMovement)<10) {
+	//		selected = true;
+	//	}
+	if (down && length(totalMovement) < 10 && downCount >= 3) {
 		float amt = mapf(downCount, 3, 12, 0.25, 1, true);
 		d.setColor(unselectedColor * (1.f - amt) + selectedColor * amt);
 	} else {
 		d.setColor(unselectedColor);
 	}
-	
-	if(down) {
+
+	if (down) {
 		downCount++;
 	}
 	Rectf r = *this;
-	r.y = (int) r.y+2;
-	r.height = (int) r.height -1;
+	r.y = (int) r.y + 2;
+	r.height = (int) r.height - 1;
 	d.drawRect(r);
-	
+
 	// this resets the button if we've lost focus (e.g. dragging has started again)
-	if(horizontalScrollTarget!=0 && !hasFocus() && !g.focusedLayers.empty()) {
+	if (horizontalScrollTarget != 0 && !hasFocus() && !g.focusedLayers.empty()) {
 		horizontalScrollTarget = 0;
 	}
-	
-	if(!settings.hasActionButton) {
+
+	if (!settings.hasActionButton) {
 		horizontalScrollTarget = std::min(0.f, horizontalScrollTarget);
 	}
 	// draw delete
 	horizontalScroll = horizontalScroll * 0.8 + horizontalScrollTarget * 0.2;
-	
+
 	// delete bg
-	if(horizontalScroll<0) {
+	if (horizontalScroll < 0) {
 		d.fill();
-		
+
 		d.setColor(settings.deleteColor);
-		
-		d.drawRect(right(), y+2, horizontalScroll, height-1);
+
+		d.drawRect(right(), y + 2, horizontalScroll, height - 1);
 		// action bg
-	} else if(settings.hasActionButton && horizontalScroll>0) {
+	} else if (settings.hasActionButton && horizontalScroll > 0) {
 		d.fill();
 		d.setColor(settings.actionColor);
-		d.drawRect(x, y+2, horizontalScroll, height-1);
+		d.drawRect(x, y + 2, horizontalScroll, height - 1);
 	}
-	
-	
+
 	d.setColor(dividerColor);
-	d.drawRect(x, (int)(y + height), width, 2);
+	d.drawRect(x, (int) (y + height), width, 2);
 }
-
-
-
 
 bool ScrollingListDeletableView::touchDown(float x, float y, int id) {
 	downCount = 0;
 	start = {x, y};
 	down = true;
 	prevTouch = start;
-//	totalYMovement = 0.f;
+	//	totalYMovement = 0.f;
 	horizontallyScrolling = false;
 	deleting = false;
 	deleteDecidey = 0.f;
@@ -117,50 +111,46 @@ bool ScrollingListDeletableView::touchDown(float x, float y, int id) {
 }
 
 void ScrollingListDeletableView::touchMoved(float x, float y, int id) {
-
 	vec2 currTouch = vec2(x, y);
 	vec2 delta = currTouch - start;
-	
-	if(horizontallyScrolling) {
+
+	if (horizontallyScrolling) {
 		// if the scroll is more than halfway left, do a full delete swipe
-		if(deleting) {
+		if (deleting) {
 			horizontalScrollTarget = delta.x;
-			
-			if(x<decidePoint -width *0.02 && !shouldDelete) {
+
+			if (x < decidePoint - width * 0.02 && !shouldDelete) {
 				shouldDelete = true;
 				haptics->lightTap();
-			} else if(x>decidePoint+width*0.02 && shouldDelete) {
+			} else if (x > decidePoint + width * 0.02 && shouldDelete) {
 				shouldDelete = false;
 				haptics->lightTap();
 			}
-		} else if(delta.x<-width*0.5) {
+		} else if (delta.x < -width * 0.5) {
 			haptics->lightTap();
 			horizontalScrollTarget = -width;
 			deleting = true;
 			shouldDelete = true;
 			start.x = width + x;
 			decidePoint = x + width * 0.05;
-//		} else if(horizontalScrollTarget>deleteButtonWidth) {
-//			horizontalScrollTarget = deleteButtonWidth;
+			//		} else if(horizontalScrollTarget>deleteButtonWidth) {
+			//			horizontalScrollTarget = deleteButtonWidth;
 		} else {
-			
 			horizontalScrollTarget = delta.x + initialScrollTarget;
-			if(settings.hasActionButton) {
-				if(horizontalScrollTarget>settings.actionButtonWidth) {
+			if (settings.hasActionButton) {
+				if (horizontalScrollTarget > settings.actionButtonWidth) {
 					horizontalScrollTarget = settings.actionButtonWidth;
 				}
 			}
 		}
 	} else {
-		
-		if(abs(delta.x)>20 && abs(delta.y)<20) {
+		if (abs(delta.x) > 20 && abs(delta.y) < 20) {
 			horizontallyScrolling = true;
-		
-		} else if(abs(delta.y)>20) {
-			
+
+		} else if (abs(delta.y) > 20) {
 			down = false;
 			localToAbsoluteCoords(x, y);
-			
+
 			Layer *scrollingList = getParent()->getParent();
 			haptics = nullptr;
 			transferFocus(scrollingList, id);
@@ -168,17 +158,15 @@ void ScrollingListDeletableView::touchMoved(float x, float y, int id) {
 			scrollingList->touchDown(x, y, id);
 		}
 	}
-	
+
 	totalMovement += (currTouch - prevTouch);
 	prevTouch = currTouch;
 }
 
 void ScrollingListDeletableView::touchUp(float x, float y, int id) {
-	if(horizontallyScrolling) {
-		
-
-		if(deleting) {
-			if(shouldDelete) {
+	if (horizontallyScrolling) {
+		if (deleting) {
+			if (shouldDelete) {
 				horizontalScrollTarget = -width;
 				collapsing = true;
 				deleteSelf();
@@ -186,32 +174,34 @@ void ScrollingListDeletableView::touchUp(float x, float y, int id) {
 				horizontalScrollTarget = 0.f;
 			}
 		} else {
-			if(settings.hasActionButton && horizontalScrollTarget>settings.actionButtonWidth/2) {
+			if (settings.hasActionButton && horizontalScrollTarget > settings.actionButtonWidth / 2) {
 				horizontalScrollTarget = settings.actionButtonWidth;
-			} else if(horizontalScrollTarget < -settings.deleteButtonWidth/2) {
-				horizontalScrollTarget = - settings.deleteButtonWidth;
+			} else if (horizontalScrollTarget < -settings.deleteButtonWidth / 2) {
+				horizontalScrollTarget = -settings.deleteButtonWidth;
 			} else {
 				horizontalScrollTarget = 0.f;
 			}
 		}
 	} else {
-		if(length(totalMovement)<10) {
-			if(horizontalScrollTarget==-settings.deleteButtonWidth) {
-				if(x>width-settings.deleteButtonWidth) {
-//					printf("delete\n");
-                    settings.dialogs.confirm("are you sure?", settings.warningDeleteMessage, [this]() {
-                        collapsing = true;
-                        deleteSelf();
-                        }, [this]() {
-                        horizontalScrollTarget = 0.f;
-                    });
+		if (length(totalMovement) < 10) {
+			if (horizontalScrollTarget == -settings.deleteButtonWidth) {
+				if (x > width - settings.deleteButtonWidth) {
+					//					printf("delete\n");
+					settings.dialogs.confirm(
+						"are you sure?",
+						settings.warningDeleteMessage,
+						[this]() {
+							collapsing = true;
+							deleteSelf();
+						},
+						[this]() { horizontalScrollTarget = 0.f; });
 
 				} else {
 					// do nothing
 				}
-				
-			} else if(settings.hasActionButton && horizontalScrollTarget==settings.actionButtonWidth) {
-				if(x<settings.actionButtonWidth) {
+
+			} else if (settings.hasActionButton && horizontalScrollTarget == settings.actionButtonWidth) {
+				if (x < settings.actionButtonWidth) {
 					action();
 					horizontalScrollTarget = 0;
 				}
