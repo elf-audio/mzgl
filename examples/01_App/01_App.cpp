eb1c4a8f8fff120ab41d5658aaea72349b29432e
Marek Bereza <bereza@gmail.com>
2023-12-07 11:18:43 +0000

    updated examples

--- examples/01_App/01_App.cpp
+++ examples/01_App/01_App.cpp
@@ -6,116 +6,96 @@
  */
 class MyApp : public App {
 public:
+	// in constructor you can allocate things but don't do anything
+	// with GPU/OpenGL stuff because the window/opengl context hasn't
+	// been created yet.
+	MyApp(Graphics &g)
+		: App(g) {}
 
-    // in constructor you can allocate things but don't do anything
-    // with GPU/OpenGL stuff because the window/opengl context hasn't
-    // been created yet.
-    MyApp(Graphics &g) : App(g) {
+	/////////////////////////////////////////////////////////////
+	// LIFE-CYCLE
 
-    }
+	// called after app is constructed and ready to draw
+	// e.g. you can allocate GPU primitives now
+	void setup() override { printf("Setup called\n"); }
 
+	// called before draw() and you can do any non graphical things here
+	// - it uses the time available in between frames.
+	void update() override {}
 
-    /////////////////////////////////////////////////////////////
-    // LIFE-CYCLE
+	void draw() override {
+		// set background colour to dark red
+		g.clear(0.2, 0, 0);
 
-    // called after app is constructed and ready to draw
-    // e.g. you can allocate GPU primitives now
-    void setup() override {
-        printf("Setup called\n");
-    }
-    
-    // called before draw() and you can do any non graphical things here
-    // - it uses the time available in between frames.
-    void update() override {
+		// set current colour to white
+		g.setColor(1);
 
-    }
+		// draw some text
+		g.drawTextCentred("move your mouse to see events in the terminal", vec2(g.width / 2, g.height / 2));
+	}
 
-    void draw() override {
-        // set background colour to dark red
-        g.clear(0.2, 0, 0);
+	// called just before the program quits - this is a time to clear
+	// everything up.
+	void exit() override { printf("exit called\n"); }
 
-        // set current colour to white
-        g.setColor(1);
+	/////////////////////////////////////////////////////////////
+	// TOUCHES
+	// these are also triggered on mouse events (id is the persistent touch id
+	// to help you identify which touch is which.)
 
-        // draw some text
-        g.drawTextCentred("move your mouse to see events in the terminal", vec2(g.width/2, g.height/2));
-    }
+	// touchOver doesn't really exist for touch devices but its
+	// a way for mouse over inputs to be received (i.e.) when you have
+	// a mouse hovering over an element rather than clicking
+	void touchOver(float x, float y) override { printf("Touch over: x: %.0f, y: %.0f\n", x, y); }
 
-    // called just before the program quits - this is a time to clear
-    // everything up.
-    void exit() override {
-        printf("exit called\n");
-    }
+	// when the touch actually touches the screen this is called
+	// (or when mouse is clicked)
+	void touchDown(float x, float y, int id) override {
+		printf("Touch down: x: %.0f, y: %.0f, id: %d\n", x, y, id);
+	}
 
-    /////////////////////////////////////////////////////////////
-    // TOUCHES
-    // these are also triggered on mouse events (id is the persistent touch id 
-    // to help you identify which touch is which.)
+	// this happens continually whilst a finger or mouse is being
+	// dragged across the screen
+	void touchMoved(float x, float y, int id) override {
+		printf("Touch moved: x: %.0f, y: %.0f, id: %d\n", x, y, id);
+	}
 
-    // touchOver doesn't really exist for touch devices but its 
-    // a way for mouse over inputs to be received (i.e.) when you have
-    // a mouse hovering over an element rather than clicking
-    void touchOver(float x, float y) override {
-        printf("Touch over: x: %.0f, y: %.0f\n", x, y);
-    }
+	// this is when the touch comes off the screen or the mouse
+	// button is released
+	void touchUp(float x, float y, int id) override { printf("Touch up: x: %.0f, y: %.0f, id: %d\n", x, y, id); }
 
-    // when the touch actually touches the screen this is called
-    // (or when mouse is clicked)
-    void touchDown(float x, float y, int id) override {
-        printf("Touch down: x: %.0f, y: %.0f, id: %d\n", x, y, id);
-    }
+	/////////////////////////////////////////////////////////////
+	// KEY EVENTS
 
-    // this happens continually whilst a finger or mouse is being
-    // dragged across the screen
-    void touchMoved(float x, float y, int id) override {
-        printf("Touch moved: x: %.0f, y: %.0f, id: %d\n", x, y, id);
-    }
+	void keyDown(int key) override { printf("key down %c\n", key); }
 
-    // this is when the touch comes off the screen or the mouse
-    // button is released
-    void touchUp(float x, float y, int id) override {
-        printf("Touch up: x: %.0f, y: %.0f, id: %d\n", x, y, id);
-    }
+	void keyUp(int key) override { printf("key up %c\n", key); }
 
-    /////////////////////////////////////////////////////////////
-    // KEY EVENTS
+	/////////////////////////////////////////////////////////////
+	// WINDOW EVENTS
 
-    void keyDown(int key) override {
-        printf("key down %c\n", key);
-    }
-
-    void keyUp(int key) override {
-        printf("key up %c\n", key);
-    }
-
-    /////////////////////////////////////////////////////////////
-    // WINDOW EVENTS
-
-    void resized() override {
-        // when resized gets called, it will have already set
-        // g.width and g.height
-        printf("new window size: %d x %d\n", g.width, g.height);
-    }
+	void resized() override {
+		// when resized gets called, it will have already set
+		// g.width and g.height
+		printf("new window size: %d x %d\n", g.width, g.height);
+	}
 };
-
 
 // every executable has instantiateApp as an entry point
 // this is called by main() which is inside the mzgl library.
 //
-// The parameter 'g' is the graphics context, of which there 
+// The parameter 'g' is the graphics context, of which there
 // must be only one per app. App needs it in its constructo
-App *instantiateApp(Graphics &g) {
+std::shared_ptr<App> instantiateApp(Graphics &g) {
+	// how to get command line args:
+	auto commandLineArgs = getCommandLineArgs();
+	for (const auto &s: commandLineArgs) {
+		printf("> %s\n", s.c_str());
+	}
 
-    // how to get command line args:
-    auto commandLineArgs = getCommandLineArgs();
-    for(const auto &s : commandLineArgs) {
-        printf("> %s\n", s.c_str());
-    }
+	// set the window width and height on the graphics class.
+	g.width	 = 800;
+	g.height = 512;
 
-
-    // set the window width and height on the graphics class.
-    g.width = 800;
-    g.height = 512;
-
-    return new MyApp(g); 
+	return std::make_shared<MyApp>(g);
 }
--- examples/04_Dialogs/04_Dialogs.cpp
+++ examples/04_Dialogs/04_Dialogs.cpp
@@ -9,144 +9,125 @@
  */
 class Button : public Layer {
 public:
-    Button(Graphics &g, const std::string &name) : Layer(g, name) {
-        interactive = true;
-    }
+	Button(Graphics &g, const std::string &name)
+		: Layer(g, name) {
+		interactive = true;
+	}
 
-    void draw() override {
+	void draw() override {
+		g.fill();
 
-        g.fill();
+		if (down) {
+			g.setColor(0);
+			g.drawRoundedRect(*this, 5);
+		} else if (over) {
+			g.setColor(0.4);
+			g.drawRoundedRect(*this, 5);
+		}
 
-        if(down) {
-            g.setColor(0);
-            g.drawRoundedRect(*this, 5);
-        } else if(over) {
-            g.setColor(0.4);
-            g.drawRoundedRect(*this, 5);
-        }
-        
+		g.setColor(1);
+		g.noFill();
+		g.drawRoundedRect(*this, 5);
 
-        g.setColor(1);
-        g.noFill();
-        g.drawRoundedRect(*this, 5);
-        
-        g.drawTextCentred(name, centre());
-    }
+		g.drawTextCentred(name, centre());
+	}
 
-    std::function<void()> released = [](){};
+	std::function<void()> released = []() {};
 
-    bool over = false;
-    bool down = false;
-    void touchOver(float x, float y) override {
-        over = inside(x, y);
-    }
-    bool touchDown(float x, float y, int id) override {
-        down = true;
-        return true;
-    }
+	bool over = false;
+	bool down = false;
+	void touchOver(float x, float y) override { over = inside(x, y); }
+	bool touchDown(float x, float y, int id) override {
+		down = true;
+		return true;
+	}
 
-    void touchMoved(float x, float y, int id) override {
-        down = inside(x, y);
-    }
-    void touchUp(float x, float y, int id) override {
-        if(inside(x, y)) {
-            released();
-        }
-        down = false;
-    }
+	void touchMoved(float x, float y, int id) override { down = inside(x, y); }
+	void touchUp(float x, float y, int id) override {
+		if (inside(x, y)) {
+			released();
+		}
+		down = false;
+	}
 };
-
-
-
 
 class MyApp : public App {
 public:
+	Dialogs dialogs;
+	MyApp(Graphics &g)
+		: App(g)
+		, dialogs(*this) {
+		auto alertButton		   = new Button(g, "SHOW ALERT");
+		auto twoOptionCancelButton = new Button(g, "SHOW TWO OPTION CANCEL");
+		auto chooseImageButton	   = new Button(g, "CHOOSE IMAGE");
+		auto shareButton		   = new Button(g, "SHARE");
+		auto loadButton			   = new Button(g, "LOAD FILE");
+		auto launchUrlButton	   = new Button(g, "LAUNCH URL");
 
-    Dialogs dialogs;
-    MyApp(Graphics &g) : App(g), dialogs(*this) {
+		////////////////////////////////////////////////////////////////////////////////////
+		// actions
 
-        auto alertButton            = new Button(g, "SHOW ALERT");
-        auto twoOptionCancelButton  = new Button(g, "SHOW TWO OPTION CANCEL");
-        auto chooseImageButton      = new Button(g, "CHOOSE IMAGE");
-        auto shareButton            = new Button(g, "SHARE");
-        auto loadButton             = new Button(g, "LOAD FILE");
-        auto launchUrlButton        = new Button(g, "LAUNCH URL");
+		alertButton->released = [this]() { dialogs.alert("TITLE", "MESSAGE"); };
 
+		twoOptionCancelButton->released = [this]() {
+			dialogs.twoOptionCancelDialog(
+				"TITLE",
+				"MSG",
+				"BUTTON ONE",
+				[this]() { dialogs.alert("", "button one pressed"); },
+				"BUTTON TWO",
+				[this]() { dialogs.alert("", "button two pressed"); },
 
+				// cancel
+				[this]() { dialogs.alert("", "cancel pressed"); });
+		};
 
-        ////////////////////////////////////////////////////////////////////////////////////
-        // actions
+		chooseImageButton->released = [this]() {
+			dialogs.chooseImage([this](bool success, std::string path) {
+				dialogs.alert("", "LOAD IMAGE - success: " + std::to_string(success) + " path: " + path);
+			});
+		};
 
-        alertButton->released = [this]() {
-            dialogs.alert("TITLE", "MESSAGE");
-        };
+		shareButton->released = [this]() {
+			dialogs.share("SHARE THIS FILE", "/path/to/real/file", [this](bool success) {
+				dialogs.alert("", "SHARE FILE - success: " + std::to_string(success));
+			});
+		};
 
+		loadButton->released = [this]() {
+			dialogs.loadFile("CHOOSE A FILE", [this](std::string path, bool success) {
+				dialogs.alert("", "LOAD FILE - success: " + std::to_string(success) + " path: " + path);
+			});
+		};
 
-        twoOptionCancelButton->released = [this]() {
-            dialogs.twoOptionCancelDialog("TITLE", "MSG",
-                                   "BUTTON ONE", [this](){dialogs.alert("", "button one pressed");},
-                                   "BUTTON TWO", [this](){dialogs.alert("", "button two pressed");},
+		launchUrlButton->released = [this]() {
+			dialogs.launchUrlInWebView("https://www.koalasampler.com", []() {});
+		};
 
-                                   // cancel
-                                   [this](){dialogs.alert("", "cancel pressed");});
-        };
+		////////////////////////////////////////////////////////////////////////////////////
+		// do the layout
+		const float buttonWidth	 = g.width * 0.5;
+		const float buttonHeight = g.height * 0.1;
+		const float padding		 = 20;
 
+		alertButton->setFromCentre({g.width / 2, buttonHeight}, buttonWidth, buttonHeight);
 
+		root->addChildren(
+			{alertButton, twoOptionCancelButton, chooseImageButton, shareButton, loadButton, launchUrlButton});
+		for (int i = 1; i < root->getNumChildren(); i++) {
+			root->getChild(i)->size(alertButton->size());
+			root->getChild(i)->positionUnder(root->getChild(i - 1), padding);
+		}
+	}
 
-
-        chooseImageButton->released = [this]() {
-            dialogs.chooseImage([this](bool success, std::string path) {
-                dialogs.alert("", "LOAD IMAGE - success: "+std::to_string(success) + " path: " + path);
-            });
-        };
-
-        shareButton->released = [this]() {
-            dialogs.share("SHARE THIS FILE", "/path/to/real/file", [this](bool success) {
-                dialogs.alert("", "SHARE FILE - success: "+std::to_string(success));
-            });
-        };
-
-
-        loadButton->released = [this]() {
-            dialogs.loadFile("CHOOSE A FILE", [this](std::string path, bool success) {
-                dialogs.alert("", "LOAD FILE - success: "+std::to_string(success) + " path: " + path);
-            });
-        };
-
-
-        launchUrlButton->released = [this]() {
-            dialogs.launchUrlInWebView("https://www.koalasampler.com", [](){});
-        };
-
-
-        ////////////////////////////////////////////////////////////////////////////////////
-        // do the layout
-        const float buttonWidth = g.width * 0.5;
-        const float buttonHeight = g.height * 0.1;
-        const float padding = 20;
-
-        alertButton->setFromCentre({g.width/2, buttonHeight}, buttonWidth, buttonHeight);
-
-
-        root->addChildren({alertButton, twoOptionCancelButton, chooseImageButton, shareButton, loadButton, launchUrlButton});
-        for(int i = 1; i < root->getNumChildren(); i++) {
-            root->getChild(i)->size(alertButton->size());
-            root->getChild(i)->positionUnder(root->getChild(i-1), padding);
-        }
-    }
-
-    void draw() override {
-        // grey background
-        g.clear(0.2);
-    }
-    
+	void draw() override {
+		// grey background
+		g.clear(0.2);
+	}
 };
 
-
-App *instantiateApp(Graphics &g) {
-
-    g.width = 800;
-    g.height = 800;
-
-    return new MyApp(g); 
+std::shared_ptr<App> instantiateApp(Graphics &g) {
+	g.width	 = 800;
+	g.height = 800;
+	return std::make_shared<MyApp>(g);
 }
