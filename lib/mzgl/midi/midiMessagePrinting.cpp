e5696a9debf5eb1392d439e80172d48123712937
Marek Bereza <bereza@gmail.com>
2024-01-04 14:09:07 +0000

    midi message debug printing and quietening rtaudio logging

--- lib/mzgl/midi/midiMessagePrinting.cpp
+++ lib/mzgl/midi/midiMessagePrinting.cpp
@@ -3,9 +3,65 @@
 //
 
 #include "midiMessagePrinting.h"
+#include <iomanip>
 
-std::ostream &operator<<(std::ostream &os, const MidiMessage &msg) {
-	os << "MidiMessage: ["
-	   << "]";
+std::string bytesToHex(const std::vector<uint8_t> &bytes) {
+	std::stringstream ss;
+	ss << std::hex << std::setfill('0');
+	for (uint8_t byte: bytes) {
+		ss << std::setw(2) << static_cast<int>(byte) << " ";
+	}
+	return ss.str();
+}
+std::ostream &operator<<(std::ostream &os, const MidiMessage &midiMsg) {
+	std::vector<uint8_t> msg = midiMsg.getBytes();
+	os << "MidiMessage: [";
+
+	if (msg.empty()) {
+		os << "Empty MIDI message.]";
+		return os;
+	}
+
+	uint8_t statusByte	= msg[0];
+	uint8_t messageType = statusByte & 0xF0;
+	uint8_t channel		= statusByte & 0x0F;
+
+	switch (messageType) {
+		case MIDI_NOTE_OFF: os << "Note Off, Channel: " << +channel; break;
+		case MIDI_NOTE_ON: os << "Note On, Channel: " << +channel; break;
+		case MIDI_POLY_AFTERTOUCH: os << "Polyphonic Key Pressure (Aftertouch), Channel: " << +channel; break;
+		case MIDI_CONTROL_CHANGE: os << "Control Change, Channel: " << +channel; break;
+		case MIDI_PROGRAM_CHANGE: os << "Program Change, Channel: " << +channel; break;
+		case MIDI_AFTERTOUCH: os << "Channel Pressure (Aftertouch), Channel: " << +channel; break;
+		case MIDI_PITCH_BEND: os << "Pitch Bend Change, Channel: " << +channel; break;
+		case MIDI_SYSEX:
+			os << "SysEx Message: " << bytesToHex(msg) << "]";
+			return os; // Early return for SysEx messages
+		// Additional System Messages
+		case MIDI_TIME_CODE:
+		case MIDI_SONG_POS_POINTER:
+		case MIDI_SONG_SELECT:
+		case MIDI_TUNE_REQUEST:
+		case MIDI_SYSEX_END:
+		case MIDI_TIME_CLOCK:
+		case MIDI_START:
+		case MIDI_CONTINUE:
+		case MIDI_STOP:
+		case MIDI_ACTIVE_SENSING:
+		case MIDI_SYSTEM_RESET:
+			// You can add specific handling for each of these cases if needed
+			os << "System Message: 0x" << std::hex << static_cast<int>(statusByte);
+			break;
+		default: os << "Unknown Message"; break;
+	}
+
+	if (msg.size() > 1) {
+		os << ", Data1: " << +msg[1];
+	}
+	if (msg.size() > 2) {
+		os << ", Data2: " << +msg[2];
+	}
+
+	os << "]";
 	return os;
 }
