84ee8cf69a96cae6f81062d59a85afb69c276cc1
Marek-mba <bereza@gmail.com>
2024-02-03 23:07:17 -0800

    clean up ScopedMask

--- lib/mzgl/gl/ScopedMask.cpp
+++ lib/mzgl/gl/ScopedMask.cpp
@@ -7,27 +7,15 @@
 //
 
 #include "ScopedMask.h"
-//#include "mzOpenGL.h"
 
 void ScopedMask::startMask(Graphics &g, const Rectf &r) {
 	masking = true;
-	//	if(glIsEnabled(GL_SCISSOR_TEST)) {
-	//		scissorWasEnabled = true;
-	//		glGetFloatv(GL_SCISSOR_BOX, vals);
-	//	}
-	//	glEnable(GL_SCISSOR_TEST);
-	//
-	//	// glScissor works with pixel coordinates
-	//	// so make sure that parameters are not
-	//	// in different coord-space. If thats the case,
-	//	// some kind of translation must take place here
-	//	// see: wrapMaskForScissors
-	//	glScissor(r.x, g.height-(r.bottom()), r.width, r.height);
-	//
+
 	graphics = &g;
 	if (graphics->isMaskOn()) {
 		scissorWasEnabled = true;
-		rect			  = graphics->getMaskRect(); //glGetFloatv(GL_SCISSOR_BOX, vals);
+
+		rect			  = graphics->getMaskRect();
 	}
 	graphics->maskOn(r);
 }
@@ -40,9 +28,5 @@ void ScopedMask::stopMask() {
 	} else {
 		graphics->maskOff();
 	}
-	//	if(scissorWasEnabled) {
-	//		glScissor(vals[0], vals[1], vals[2], vals[3]);
-	//	} else {
-	//		glDisable(GL_SCISSOR_TEST);
-	//	}
+
 }
