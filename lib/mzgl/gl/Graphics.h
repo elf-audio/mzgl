b7c23936f210691a5d400e6f51ddc5f0151e260e
Marek Bereza <bereza@gmail.com>
2024-05-17 11:40:55 +0100

    add sokol  stuff from research project
    shaders now compile in cmakelists and whole thing compiles but it crashes obvously
    
    move sampler registry into its own class
    
    abstracted the textures
    
    OpenGL texture still works with new abstraction!
    
    now we get a metal view and no crash but no drawing
    
    sokol setup code
    
    add sokol  stuff from research project
    
    shaders now compile in cmakelists and whole thing compiles but it crashes obvously
    
    move sampler registry into its own class
    
    abstracted the textures
    
    OpenGL texture still works with new abstraction!
    
    now we get a metal view and no crash but no drawing
    
    work in progress
    
    vbo impl mostly done, bit of a tidy and speed up mvp uniform a bit
    
    made it build for iOS
    
    xcode flag
    
    opengl shader abstracted
    
    Vbo now uses inheritance rather than impl
    
    Moved Texture to inhertance from pimpl
    
    removed TextureImpl
    
    and forward decl
    
    starting to roll sokol back in
    
    working on getting sokol shader to compile
    
    sokol shader wip
    
    something actually runs
    
    fixes a load of things, looks better, still a long way to go
    
    pipeline will now do primitive modes (not fully working)
    
    font almost works, just got alpha problem
    
    fixed font rendering and got rid of line loop and triangle fan primitive modes
    
    fixed the buffer leak, don't know why tho
    
    shaders can do uniform arrays now
    
    working on instancing - something seems to be not going right
    
    instancing now works
    
    TextureBinding and shaders for xcode, temporarily
    
    cleaned up compileShader for opengl

--- lib/mzgl/gl/Graphics.h
+++ lib/mzgl/gl/Graphics.h
@@ -70,6 +70,7 @@ class Graphics {
 public:
 	Graphics();
 	~Graphics();
+	void init();
 	int width			 = 0;
 	int height			 = 0;
 	float pixelScale	 = 2.f;
@@ -225,10 +226,12 @@ public:
 
 	// OpenGL only
 	int32_t getDefaultFrameBufferId();
+	GraphicsAPI &getAPI() { return *api; }
 
 private:
-	float strokeWeight	 = 1;
-	BlendMode blendMode	 = BlendMode::Alpha;
+	float strokeWeight	= 1;
+	BlendMode blendMode = BlendMode::Alpha;
+
 	bool blendingEnabled = false;
 	bool filling		 = true;
 	glm::vec4 color;
