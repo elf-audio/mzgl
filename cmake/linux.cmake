57724f52bbc6111a15d9e279781bc8fed4022a66
Marek Bereza <marek@elf-audio.com>
2024-06-24 16:18:09 +0100

    linux

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -1,26 +1,23 @@
 if (LINUX)
   add_compile_definitions (__LINUX_ALSA__)
+  find_package (ALSA REQUIRED)
 
   # -mfpu=neon-vfpv4
   if (__ARMEL__)
     add_compile_options (-mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon -O3)
   endif (__ARMEL__)
 
-  find_package (ALSA REQUIRED)
   find_package (PkgConfig REQUIRED)
-  find_library (JACK_LIB jack)
-  find_package (PkgConfig)
-  find_package (OpenGL REQUIRED)
-  find_package (X11 REQUIRED)
-
-  pkg_check_modules (GLFW REQUIRED glfw3)
   pkg_check_modules (GTK REQUIRED gtk+-3.0)
-  pkg_check_modules (jack jack)
 
   include_directories (${GTK_INCLUDE_DIRS})
   link_directories (${GTK_LIBRARY_DIRS})
 
   add_definitions (${GTK_CFLAGS_OTHER})
+
+  find_library (JACK_LIB jack)
+  find_package (PkgConfig)
+  pkg_check_modules (jack jack)
 
   if (JACK_LIB OR jack_FOUND)
     set (HAVE_JACK TRUE)
@@ -36,8 +33,26 @@ if (LINUX)
     list (APPEND MZGL_LIBRARIES ${jack_LINK_LIBRARIES})
   endif ()
 
-  include_directories (${OPENGL_INCLUDE_DIRS} ${X11_INCLUDE_DIR} ${GLFW_INCLUDE_DIRS} "/usr/include")
-  link_directories (${GTK_LIBRARY_DIRS} ${GLFW_LIBRARY_DIRS} "/usr/lib/x86_64-linux-gnu")
+  find_package (OpenGL REQUIRED)
+  find_package (X11 REQUIRED)
+#  find_package (glfw3 3.3 REQUIRED)
+
+
+pkg_check_modules(GLFW REQUIRED glfw3)
+
+#  if (NOT glfw3_FOUND)
+#    message (STATUS "GLFW not found, setting manually.")
+#    set (GLFW3_INCLUDE_DIR "/usr/include/GLFW")
+#    set (GLFW3_LIBRARY "/usr/lib/x86_64-linux-gnu/libglfw.so")
+#    include_directories (${GLFW3_INCLUDE_DIR})
+#    link_directories (${GLFW3_LIBRARY})
+#  else ()
+#    include_directories (${glfw3_INCLUDE_DIRS})
+#    link_directories (${glfw3_LIBRARY_DIRS})
+#  endif ()
+
+  include_directories (${OPENGL_INCLUDE_DIRS} ${X11_INCLUDE_DIR}  ${GLFW_INCLUDE_DIRS} "/usr/include")
+  link_directories(${GTK_LIBRARY_DIRS} ${GLFW_LIBRARY_DIRS} "/usr/lib/x86_64-linux-gnu")
 
   list (
     APPEND
@@ -46,6 +61,5 @@ if (LINUX)
     ${ALSA_LIBRARIES}
     ${OPENGL_LIBRARIES}
     ${X11_LIBRARIES}
-    ${GLFW3_LIBRARY}
-    glfw)
+    ${GLFW3_LIBRARIES} glfw)
 endif ()
--- lib/mzgl/app/glfw/GLFWAppRunner.cpp
+++ lib/mzgl/app/glfw/GLFWAppRunner.cpp
@@ -6,19 +6,22 @@
 //  Copyright © 2018 Marek Bereza. All rights reserved.
 //
 
-#ifdef _WIN32
+#ifdef _WIN32 ////////////////////////////////////////////////
+
+// This enables visual styles on Windows
 #	pragma comment(linker, "\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #	include <glew.h>
-#endif
+#endif // _WIN32 /////////////////////////////////////////////
 
-#ifdef __linux__
-#	include <GLFW / glfw3.h>
+#ifdef __linux__ /////////////////////////////////////////////
+#	include <gtk/gtk.h>
+#include <GLFW/glfw3.h>
 #else
-#	include <glfw / glfw3.h>
-#endif
+#include <glfw/glfw3.h>
+#endif // __linux__ //////////////////////////////////////////
 
 #include "GLFWAppRunner.h"
 #include <optional>
