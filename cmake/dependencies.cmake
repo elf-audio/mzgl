ec08b7467c6d0a9a7858817e5e57e31f3293c5ee
Paul Chana <paul@elf-audio.com>
2024-10-10 16:56:43 +0100

    Add fast tri and fontstash

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,5 +3,7 @@ function(mzgl_add_packages)
   mzgl_add_package("gh:nlohmann/json@3.11.3")
   mzgl_add_package("gh:cameron314/concurrentqueue@1.0.4")
   mzgl_add_package("gh:cameron314/readerwriterqueue@1.0.6")
+  mzgl_add_package("gh:elf-audio/fontstash#f49c10f")
+  mzgl_add_package("gh:elf-audio/fast-poly2tri#5dcc516")
 
 endfunction()
