673c679c1441aeeebe3207bf46bb4ba6a95ac4f3
Marek Bereza <bereza@gmail.com>
2025-03-24 17:04:28 +0000

    enable flags in debug

--- lib/mzgl/util/util.cpp
+++ lib/mzgl/util/util.cpp
@@ -263,7 +263,7 @@ void addCommandLineFlag(const std::string &flag) {
 }
 
 bool hasCommandLineFlag(const std::string &flag) {
-#ifdef MZGL_MAC
+#if MZGL_MAC || defined(DEBUG)
 	auto args = getCommandLineArgs();
 	return std::find_if(std::begin(args), std::end(args), [flag](auto &&arg) { return arg == flag; })
 		   != std::end(args);
