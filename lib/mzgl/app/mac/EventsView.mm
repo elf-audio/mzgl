f50b64c492d73763e1f0ad907454628e1f0e4f10
Marek Bereza <bereza@gmail.com>
2025-06-04 18:25:29 +0100

    right and middle mouse button support (mac only)

--- lib/mzgl/app/mac/EventsView.mm
+++ lib/mzgl/app/mac/EventsView.mm
@@ -146,24 +146,41 @@ int nsEventToKey(NSEvent *evt) {
 - (void)mouseDown:(NSEvent *)event {
 	auto mouse = [self transformMouse:event];
 	int id	   = (int) [event buttonNumber];
-	eventDispatcher->app->main.runOnMainThread(
-		true, [self, mouse, id]() { eventDispatcher->touchDown(mouse.x, mouse.y, id); });
+	eventDispatcher->app->main.runOnMainThread(true, [self, mouse, id]() {
+		static constexpr int Left  = 0;
+		static constexpr int Right = 1;
+
+		if (id == Left) eventDispatcher->touchDown(mouse.x, mouse.y, 0);
+		else if (id == Right) eventDispatcher->touchDown(mouse.x, mouse.y, RightMouseButton);
+		else eventDispatcher->touchDown(mouse.x, mouse.y, MiddleMouseButton);
+	});
 	NSEventDispatcher::instance().dispatch(event, self);
 }
 
 - (void)mouseUp:(NSEvent *)event {
 	auto mouse = [self transformMouse:event];
 	int id	   = (int) [event buttonNumber];
-	eventDispatcher->app->main.runOnMainThread(
-		true, [self, mouse, id]() { eventDispatcher->touchUp(mouse.x, mouse.y, id); });
+	eventDispatcher->app->main.runOnMainThread(true, [self, mouse, id]() {
+		static constexpr int Left  = 0;
+		static constexpr int Right = 1;
+		if (id == Left) eventDispatcher->touchUp(mouse.x, mouse.y, 0);
+		else if (id == Right) eventDispatcher->touchUp(mouse.x, mouse.y, RightMouseButton);
+		else eventDispatcher->touchUp(mouse.x, mouse.y, MiddleMouseButton);
+	});
 	NSEventDispatcher::instance().dispatch(event, self);
 }
 
 - (void)mouseDragged:(NSEvent *)event { // Mouse click-and-drag
 	auto mouse = [self transformMouse:event];
 	int id	   = (int) [event buttonNumber];
-	eventDispatcher->app->main.runOnMainThread(
-		true, [self, mouse, id]() { eventDispatcher->touchMoved(mouse.x, mouse.y, id); });
+
+	eventDispatcher->app->main.runOnMainThread(true, [self, mouse, id]() {
+		static constexpr int Left  = 0;
+		static constexpr int Right = 1;
+		if (id == Left) eventDispatcher->touchMoved(mouse.x, mouse.y, 0);
+		else if (id == Right) eventDispatcher->touchMoved(mouse.x, mouse.y, RightMouseButton);
+		else eventDispatcher->touchMoved(mouse.x, mouse.y, MiddleMouseButton);
+	});
 	NSEventDispatcher::instance().dispatch(event, self);
 }
 
