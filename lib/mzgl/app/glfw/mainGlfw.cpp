9698bda409704bb97ff375c4f389bac2451e921b
Marek Bereza <bereza@gmail.com>
2025-08-27 08:19:25 +0100

    test for seeing if we can get console output when started from terminal

--- lib/mzgl/app/glfw/mainGlfw.cpp
+++ lib/mzgl/app/glfw/mainGlfw.cpp
@@ -3,8 +3,21 @@
 #ifdef _WIN32
 #	include <windows.h>
 
+inline void AttachOrAllocConsoleForLogs(bool alloc_if_needed = false) {
+	if (AttachConsole(ATTACH_PARENT_PROCESS) || (alloc_if_needed && AllocConsole())) {
+		FILE *f;
+		freopen_s(&f, "CONOUT$", "w", stdout);
+		freopen_s(&f, "CONOUT$", "w", stderr);
+		freopen_s(&f, "CONIN$", "r", stdin);
+		setvbuf(stdout, nullptr, _IONBF, 0);
+		setvbuf(stderr, nullptr, _IONBF, 0);
+	}
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	int argc = 1;
+	// Attach to the terminal if one exists. No console is created when launched from Explorer.
+	AttachOrAllocConsoleForLogs(false);
 	//	char *argv[1];
 	//	argv[0] = "mzgl";
 	//
