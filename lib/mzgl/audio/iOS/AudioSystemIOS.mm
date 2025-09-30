d391da8dc8e17bd610bf6f318315be9fdbd050b9
Paul Chana <paul@elf-audio.com>
2025-09-30 14:57:43 +0100

    Rmove logging message

--- lib/mzgl/audio/iOS/AudioSystemIOS.mm
+++ lib/mzgl/audio/iOS/AudioSystemIOS.mm
@@ -169,7 +169,7 @@ static void routeChangeNotificationHandler(CFNotificationCenterRef center,
 }
 void AudioSystemIOS::handleRouteChange() {
 	//	Log::w() << "Got route change notification - should re-query samplerate";
-	Log::d() << "Route changed to " << getInput().name;
+//	Log::d() << "Route changed to " << getInput().name;
 }
 
 AudioSystemIOS::AudioSystemIOS() {
