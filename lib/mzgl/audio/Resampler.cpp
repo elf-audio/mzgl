8f28fc13ca249a3522311adac0f60479400bffa2
Marek Bereza <bereza@gmail.com>
2026-07-08 15:24:20 +0100

    compile warning

--- lib/mzgl/audio/Resampler.cpp
+++ lib/mzgl/audio/Resampler.cpp
@@ -32,8 +32,8 @@ void Resampler::process(const vector<float> &ins, vector<float> &outs) {
 	int outNumberOfSamples = (int) ((double) (ins.size() + 2) * ratio);
 	if (outs.size() < outNumberOfSamples) outs.resize(outNumberOfSamples);
 
-	spx_uint32_t inFrames  = ins.size() / numChannels;
-	spx_uint32_t outFrames = outs.size() / numChannels;
+	auto inFrames  = static_cast<spx_uint32_t>(ins.size() / numChannels);
+	auto outFrames = static_cast<spx_uint32_t>(outs.size() / numChannels);
 	int error =
 		speex_resampler_process_interleaved_float(resampler, ins.data(), &inFrames, outs.data(), &outFrames);
 	if (outFrames * numChannels != outs.size()) outs.resize(outFrames * numChannels);
