9d4a3c1eb3b6cebc595021b223266c44170e5ebe
Marek Bereza <bereza@gmail.com>
2026-09-12 12:45:21 +0100

    add arc for MacFilePickerDelegate and remove juce audio system

--- lib/mzgl/audio/juce/lib/mzgl_juce_CoreAudio.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "microjuce.h"
-#include "juce_BigInteger.h"
-#include "juce_AudioIODevice.h"
-#include "juce_AudioIODeviceType.h"
-#include <memory>
-
-std::shared_ptr<juce::AudioIODeviceType> createJuceIODeviceType();
